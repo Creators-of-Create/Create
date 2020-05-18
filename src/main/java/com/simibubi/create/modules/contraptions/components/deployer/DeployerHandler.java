e3d3a5de3d7f3b5adc353fd1ce6bd89f07ef6eeb
Snownee <1850986885@qq.com>
2020-05-18 18:34:02 +0800

    Fix deployer cannot place potato on campfire
    https://discordapp.com/channels/620934202875183104/689866656914210897/697585687397925007

--- src/main/java/com/simibubi/create/modules/contraptions/components/deployer/DeployerHandler.java
+++ src/main/java/com/simibubi/create/modules/contraptions/components/deployer/DeployerHandler.java
@@ -82,7 +82,7 @@ public class DeployerHandler {
 
 	static boolean shouldActivate(ItemStack held, World world, BlockPos targetPos) {
 		if (held.getItem() instanceof BlockItem)
-			if (!world.getBlockState(targetPos).getMaterial().isReplaceable())
+			if (world.getBlockState(targetPos).getBlock() == ((BlockItem) held.getItem()).getBlock())
 				return false;
 
 		if (held.getItem() instanceof BucketItem) {
