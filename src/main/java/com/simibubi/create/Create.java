8cf8dbaf740a512257e44bb68f86ee145cdcc7c7
simibubi <31564874+simibubi@users.noreply.github.com>
2020-06-08 22:16:06 +0200

    Version bump

--- src/main/java/com/simibubi/create/Create.java
+++ src/main/java/com/simibubi/create/Create.java
@@ -38,7 +38,7 @@ public class Create {
 
 	public static final String ID = "create";
 	public static final String NAME = "Create";
-	public static final String VERSION = "0.2.4";
+	public static final String VERSION = "0.2.4a";
 
 	public static Logger logger = LogManager.getLogger();
 	public static ItemGroup baseCreativeTab = new CreateItemGroup();
