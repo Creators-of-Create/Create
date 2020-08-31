53e0c61da7a681fb5125db026a1f11c3e1cccca8
Zelophed <zefren1@googlemail.com>
2020-08-31 16:01:26 +0200

    Mechanical Arm Round Robin
    - added a scroll option to the arm that enables round robin for in- and output

--- src/main/java/com/simibubi/create/content/logistics/block/mechanicalArm/ArmTileEntity.java
+++ src/main/java/com/simibubi/create/content/logistics/block/mechanicalArm/ArmTileEntity.java
@@ -1,18 +1,18 @@
 package com.simibubi.create.content.logistics.block.mechanicalArm;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.Nullable;
-
 import com.simibubi.create.content.contraptions.base.KineticTileEntity;
 import com.simibubi.create.content.logistics.block.mechanicalArm.ArmInteractionPoint.Jukebox;
 import com.simibubi.create.content.logistics.block.mechanicalArm.ArmInteractionPoint.Mode;
 import com.simibubi.create.foundation.advancement.AllTriggers;
+import com.simibubi.create.foundation.gui.AllIcons;
 import com.simibubi.create.foundation.gui.widgets.InterpolatedAngle;
+import com.simibubi.create.foundation.tileEntity.TileEntityBehaviour;
+import com.simibubi.create.foundation.tileEntity.behaviour.CenteredSideValueBoxTransform;
+import com.simibubi.create.foundation.tileEntity.behaviour.scrollvalue.INamedIconOptions;
+import com.simibubi.create.foundation.tileEntity.behaviour.scrollvalue.ScrollOptionBehaviour;
 import com.simibubi.create.foundation.utility.AngleHelper;
+import com.simibubi.create.foundation.utility.Lang;
 import com.simibubi.create.foundation.utility.NBTHelper;
-
 import net.minecraft.block.BlockState;
 import net.minecraft.block.JukeboxBlock;
 import net.minecraft.item.ItemStack;
@@ -20,8 +20,13 @@ import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.INBT;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.common.util.Constants.NBT;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArmTileEntity extends KineticTileEntity {
 
@@ -46,6 +51,11 @@ public class ArmTileEntity extends KineticTileEntity {
 	float previousBaseAngle;
 	boolean updateInteractionPoints;
 
+	//
+	protected ScrollOptionBehaviour<SelectionMode> selectionMode;
+	protected int lastInputIndex = -1;
+	protected int lastOutputIndex = -1;
+
 	enum Phase {
 		SEARCH_INPUTS, MOVE_TO_INPUT, SEARCH_OUTPUTS, MOVE_TO_OUTPUT, DANCING
 	}
@@ -64,6 +74,18 @@ public class ArmTileEntity extends KineticTileEntity {
 		previousTarget = ArmAngleTarget.NO_TARGET;
 		previousBaseAngle = previousTarget.baseAngle;
 		updateInteractionPoints = true;
+	}
+
+	@Override
+	public void addBehaviours(List<TileEntityBehaviour> behaviours) {
+		super.addBehaviours(behaviours);
+
+		selectionMode = new ScrollOptionBehaviour<>(SelectionMode.class, Lang.translate("mechanical_arm.selection_mode"), this,
+				new CenteredSideValueBoxTransform((blockState, direction) -> {
+					return direction != Direction.DOWN && direction != Direction.UP;
+				}));
+		selectionMode.requiresWrench();
+		behaviours.add(selectionMode);
 	}
 
 	@Override
@@ -163,37 +185,80 @@ public class ArmTileEntity extends KineticTileEntity {
 	}
 
 	protected void searchForItem() {
-		for (int index = 0; index < inputs.size(); index++) {
-			ArmInteractionPoint armInteractionPoint = inputs.get(index);
-			for (int i = 0; i < armInteractionPoint.getSlotCount(world); i++) {
-				if (getDistributableAmount(armInteractionPoint, i) == 0)
+		boolean foundInput = false;
+		//for round robin, we start looking after the last used index, for default we start at 0;
+		int startIndex = selectionMode.get() == SelectionMode.DEFAULT ? 0 : lastInputIndex + 1;
+
+		//if we enforce round robin, only look at the next input in the list, otherwise, look at all inputs
+		int scanRange = selectionMode.get() == SelectionMode.ROUND_ROBIN_HARD ? lastInputIndex + 2 : inputs.size();
+		if (scanRange > inputs.size())
+			scanRange = inputs.size();
+
+		InteractionPoints: for (int i = startIndex; i < scanRange; i++) {
+			ArmInteractionPoint armInteractionPoint = inputs.get(i);
+			for (int j = 0; j < armInteractionPoint.getSlotCount(world); j++) {
+				if (getDistributableAmount(armInteractionPoint, j) == 0)
 					continue;
 
-				phase = Phase.MOVE_TO_INPUT;
-				chasedPointIndex = index;
-				chasedPointProgress = 0;
-				sendData();
-				markDirty();
-				return;
+				selectIndex(true, i);
+				foundInput = true;
+				break InteractionPoints;
 			}
+		}
+		if (!foundInput && selectionMode.get() == SelectionMode.ROUND_ROBIN_SOFT) {
+			//if we didn't find an input, but don't want to enforce round robin, reset the last index
+			lastInputIndex = -1;
+		}
+		if (lastInputIndex == inputs.size() - 1) {
+			//if we reached the last input in the list, reset the last index
+			lastInputIndex = -1;
 		}
 	}
 
 	protected void searchForDestination() {
 		ItemStack held = heldItem.copy();
-		for (int index = 0; index < outputs.size(); index++) {
-			ArmInteractionPoint armInteractionPoint = outputs.get(index);
+
+		boolean foundOutput = false;
+		//for round robin, we start looking after the last used index, for default we start at 0;
+		int startIndex = selectionMode.get() == SelectionMode.DEFAULT ? 0 : lastOutputIndex + 1;
+
+		//if we enforce round robin, only look at the next index in the list, otherwise, look at all
+		int scanRange = selectionMode.get() == SelectionMode.ROUND_ROBIN_HARD ? lastOutputIndex + 2 : outputs.size();
+		if (scanRange > outputs.size())
+			scanRange = outputs.size();
+
+		for (int i = startIndex; i < scanRange; i++) {
+			ArmInteractionPoint armInteractionPoint = outputs.get(i);
 			ItemStack remainder = armInteractionPoint.insert(world, held, true);
 			if (remainder.equals(heldItem, false))
 				continue;
 
-			phase = Phase.MOVE_TO_OUTPUT;
-			chasedPointIndex = index;
-			chasedPointProgress = 0;
-			sendData();
-			markDirty();
-			return;
+			selectIndex(false, i);
+			foundOutput = true;
+			break;
 		}
+
+		if (!foundOutput && selectionMode.get() == SelectionMode.ROUND_ROBIN_SOFT) {
+			//if we didn't find an input, but don't want to enforce round robin, reset the last index
+			lastOutputIndex = -1;
+		}
+		if (lastOutputIndex == outputs.size() - 1) {
+			//if we reached the last input in the list, reset the last index
+			lastOutputIndex = -1;
+		}
+	}
+
+	//input == true => select input, false => select output
+	private void selectIndex(boolean input, int index) {
+		phase = input ? Phase.MOVE_TO_INPUT : Phase.MOVE_TO_OUTPUT;
+		chasedPointIndex = index;
+		chasedPointProgress = 0;
+		if (input)
+			lastInputIndex = index;
+		else
+			lastOutputIndex = index;
+		sendData();
+		markDirty();
 	}
 
 	protected int getDistributableAmount(ArmInteractionPoint armInteractionPoint, int i) {
@@ -319,6 +384,32 @@ public class ArmTileEntity extends KineticTileEntity {
 				previousPoint == null ? ArmAngleTarget.NO_TARGET : previousPoint.getTargetAngles(pos, ceiling);
 			if (previousPoint != null)
 				previousBaseAngle = previousPoint.getTargetAngles(pos, ceiling).baseAngle;
+		}
+	}
+
+	public enum SelectionMode implements INamedIconOptions {
+		DEFAULT(AllIcons.I_TOOL_MIRROR),//first valid interaction points gets used
+		ROUND_ROBIN_SOFT(AllIcons.I_TOOL_ROTATE),//attempt round robin, but skip invalid points
+		ROUND_ROBIN_HARD(AllIcons.I_TOOL_ROTATE),//enforce round robin, wait for invalid points to be ready again
+
+		;
+
+		private final String translationKey;
+		private final AllIcons icon;
+
+		SelectionMode(AllIcons icon) {
+			this.icon = icon;
+			this.translationKey = "mechanical_arm.selection_mode." + Lang.asId(name());
+		}
+
+		@Override
+		public AllIcons getIcon() {
+			return icon;
+		}
+
+		@Override
+		public String getTranslationKey() {
+			return translationKey;
 		}
 	}
 
