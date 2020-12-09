5f941808075f61683c763eaf3cc9005894c938b7
simibubi <31564874+simibubi@users.noreply.github.com>
2020-12-09 18:52:57 +0100

    Revert "Underwater Wheels"

--- src/main/java/com/simibubi/create/content/contraptions/components/waterwheel/WaterWheelBlock.java
+++ src/main/java/com/simibubi/create/content/contraptions/components/waterwheel/WaterWheelBlock.java
@@ -13,7 +13,6 @@ import com.simibubi.create.foundation.utility.worldWrappers.WrappedWorld;
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BlockItemUseContext;
@@ -27,8 +26,6 @@ import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
-
-import static net.minecraft.block.BubbleColumnBlock.DRAG;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
@@ -97,17 +94,7 @@ public class WaterWheelBlock extends HorizontalKineticBlock implements ITE<Water
 		boolean clockwise = wf.getAxisDirection() == AxisDirection.POSITIVE;
 		int clockwiseMultiplier = 2;
 
-		Vector3d vec;
-		if (f.getAxis().isHorizontal()) {
-			BlockState adjacentBlock = world.getBlockState(pos.offset(f));
-			if (adjacentBlock.getBlock() == Blocks.BUBBLE_COLUMN.getBlock()) {
-				vec = new Vector3d(0.0, adjacentBlock.get(DRAG) ? -1.0 : 1.0, 0.0);
-			} else {
-				vec = fluid.getFlow(world, pos.offset(f));
-			}
-		} else {
-			vec = fluid.getFlow(world, pos.offset(f));
-		}
+		Vector3d vec = fluid.getFlow(world, pos.offset(f));
 		vec = vec.scale(f.getAxisDirection()
 			.getOffset());
 		vec = new Vector3d(Math.signum(vec.x), Math.signum(vec.y), Math.signum(vec.z));
