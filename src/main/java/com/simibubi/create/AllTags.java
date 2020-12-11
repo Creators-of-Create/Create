1a052d1ec10786df998634c83b34ab3c341a6912
Snownee <mjy523@126.com>
2020-12-12 05:00:25 +0800

    Tag data-gen

--- src/main/java/com/simibubi/create/AllTags.java
+++ src/main/java/com/simibubi/create/AllTags.java
@@ -163,5 +163,7 @@ public class AllTags {
 		AllBlockTags.FAN_TRANSPARENT.add(Blocks.IRON_BARS);
 
 		AllBlockTags.FAN_HEATERS.add(Blocks.MAGMA_BLOCK, Blocks.CAMPFIRE, Blocks.LAVA, Blocks.FIRE);
+
+		AllBlockTags.SAFE_NBT.includeAll(BlockTags.SIGNS);
 	}
 }
