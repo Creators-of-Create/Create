2b615b68bdfa57a7702246f2c58f7335bb3453bb
grimmauld <soeren@benjos.de>
2021-01-13 09:50:00 +0100

    Fix mixins outside dev env

--- src/main/java/com/simibubi/create/foundation/mixin/RenderInLayerMixin.java
+++ src/main/java/com/simibubi/create/foundation/mixin/RenderInLayerMixin.java
@@ -1,20 +1,18 @@
 package com.simibubi.create.foundation.mixin;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import net.minecraft.client.multiplayer.ClientChunkProvider;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.WorldRenderer;
-import net.minecraft.util.math.SectionPos;
-import net.minecraft.world.LightType;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class RenderInLayerMixin {
-
-    @Inject(at = @At("HEAD"), method = "renderLayer")
+    @Shadow
+    @Inject(at = @At("HEAD"), method = "renderLayer")  // (Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/matrix/MatrixStack;DDD)V")
     private void renderLayer(RenderType type, MatrixStack stack, double cameraX, double cameraY, double cameraZ, CallbackInfo ci) {
 
     }
