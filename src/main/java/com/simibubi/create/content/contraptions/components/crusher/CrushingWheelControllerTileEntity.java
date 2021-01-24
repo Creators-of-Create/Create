cb25c5ab21cc710d0716ca9fe3c198c5628a190b
_0Steven <42909981+SuicidalSteve@users.noreply.github.com>
2021-01-24 16:46:07 +0100

    Omnidirectional Crushing
    -Fix directionality of horizontally placed crushing wheels.
    -Allow crushing wheels to work omnidirectionally
    -Allow downwards crushers to deposit directly into belts/chutes/depots and such below.
    -Allow horizontal crushers to take input from belts going below them.
    -Allow horizontal crushers to deposit directly into belts/chutes/depots and such placed below, offset by one in the output direction.
    -Crushers stall when they have a valid output block but cannot actually output.
    -Crushers outputting in any direction except down will shoot out the item entities with some speed.
    -Crushers can take entity input from their corresponding input directions rather than just up. However, outputting the entity drops in the right location does currently not work consistently.

--- src/main/java/com/simibubi/create/content/contraptions/components/crusher/CrushingWheelControllerTileEntity.java
+++ src/main/java/com/simibubi/create/content/contraptions/components/crusher/CrushingWheelControllerTileEntity.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import java.util.Random;
 import java.util.UUID;
 
+import static com.simibubi.create.content.contraptions.components.crusher.CrushingWheelControllerBlock.FACING;
+
 import com.simibubi.create.AllRecipeTypes;
 import com.simibubi.create.content.contraptions.processing.ProcessingInventory;
 import com.simibubi.create.content.contraptions.processing.ProcessingRecipe;
@@ -16,6 +18,7 @@ import com.simibubi.create.foundation.tileEntity.TileEntityBehaviour;
 import com.simibubi.create.foundation.tileEntity.behaviour.belt.DirectBeltInputBehaviour;
 import com.simibubi.create.foundation.utility.VecHelper;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.BlockItem;
@@ -28,7 +31,9 @@ import net.minecraft.particles.ItemParticleData;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.Direction;
+import net.minecraft.util.Direction.Axis;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.common.capabilities.Capability;
@@ -63,7 +68,15 @@ public class CrushingWheelControllerTileEntity extends SmartTileEntity {
 
 	@Override
 	public void addBehaviours(List<TileEntityBehaviour> behaviours) {
-		behaviours.add(new DirectBeltInputBehaviour(this));
+		behaviours.add(new DirectBeltInputBehaviour(this).onlyInsertWhen(this::supportsDirectBeltInput));
+	}
+
+	private boolean supportsDirectBeltInput(Direction side) {
+		BlockState blockState = getBlockState();
+		if (blockState == null)
+			return false;
+		Direction direction = blockState.get(CrushingWheelControllerBlock.FACING);
+		return direction == Direction.DOWN || direction == side;
 	}
 
 	@Override
@@ -72,12 +85,14 @@ public class CrushingWheelControllerTileEntity extends SmartTileEntity {
 		if (searchForEntity) {
 			searchForEntity = false;
 			List<Entity> search = world.getEntitiesInAABBexcluding(null, new AxisAlignedBB(getPos()),
-				e -> entityUUID.equals(e.getUniqueID()));
+					e -> entityUUID.equals(e.getUniqueID()));
 			if (search.isEmpty())
 				clear();
 			else
 				processingEntity = search.get(0);
 		}
+
+
 
 		if (!isOccupied())
 			return;
@@ -85,13 +100,22 @@ public class CrushingWheelControllerTileEntity extends SmartTileEntity {
 			return;
 
 		float speed = crushingspeed * 4;
-		Vec3d outPos = VecHelper.getCenterOf(pos);
+
+		Vec3d centerPos = VecHelper.getCenterOf(pos);
+		Direction facing = getBlockState().get(FACING);
+		int offset = facing.getAxisDirection().getOffset();
+		Vec3d outSpeed = new Vec3d((facing.getAxis() == Axis.X ? 0.25D : 0.0D) * offset
+				, offset == 1 ? (facing.getAxis() == Axis.Y ? 0.5D : 0.0D) : 0.0D		//Increased upwards speed so upwards crushing wheels shoot out the item properly.
+				, (facing.getAxis() == Axis.Z ? 0.25D : 0.0D) * offset);		//No downwards speed, so downwards crushing wheels drop the items as before.
+		Vec3d outPos = centerPos.add((facing.getAxis() == Axis.X ? .55f * offset : 0f)
+				, (facing.getAxis() == Axis.Y ? .55f * offset : 0f)
+				, (facing.getAxis() == Axis.Z ? .55f * offset : 0f));
 
 		if (!hasEntity()) {
 
 			float processingSpeed =
-				MathHelper.clamp((speed) / (!inventory.appliedRecipe ? MathHelper.log2(inventory.getStackInSlot(0)
-					.getCount()) : 1), .25f, 20);
+					MathHelper.clamp((speed) / (!inventory.appliedRecipe ? MathHelper.log2(inventory.getStackInSlot(0)
+							.getCount()) : 1), .25f, 20);
 			inventory.remainingTime -= processingSpeed;
 			spawnParticles(inventory.getStackInSlot(0));
 
@@ -105,61 +129,111 @@ public class CrushingWheelControllerTileEntity extends SmartTileEntity {
 				return;
 			}
 
-			if (inventory.remainingTime <= 0) {
-				for (int slot = 0; slot < inventory.getSlots(); slot++) {
-					ItemStack stack = inventory.getStackInSlot(slot);
-					if (stack.isEmpty())
-						continue;
-					ItemEntity entityIn = new ItemEntity(world, outPos.x, outPos.y, outPos.z, stack);
-					entityIn.setMotion(Vec3d.ZERO);
-					entityIn.getPersistentData()
-						.put("BypassCrushingWheel", NBTUtil.writeBlockPos(pos));
-					world.addEntity(entityIn);
-				}
-				inventory.clear();
-				world.notifyBlockUpdate(pos, getBlockState(), getBlockState(), 2 | 16);
+			if (inventory.remainingTime > 0) {
 				return;
 			}
+			inventory.remainingTime = 0;
+
+			//Output Items
+			if (facing.getAxis().isHorizontal() || facing == Direction.DOWN) {
+				BlockPos nextPos = pos.add(facing.getAxis() == Axis.X ? 1f * offset : 0f
+						, (facing == Direction.DOWN || facing.getAxis().isHorizontal()) ? -1f : 0f
+						, facing.getAxis() == Axis.Z ? 1f * offset : 0f);
+				DirectBeltInputBehaviour behaviour = TileEntityBehaviour.get(world, nextPos, DirectBeltInputBehaviour.TYPE);
+				if (behaviour != null) {
+					boolean changed = false;
+					if (!behaviour.canInsertFromSide(facing))
+						return;
+					for (int slot = 0; slot < inventory.getSlots(); slot++) {
+						ItemStack stack = inventory.getStackInSlot(slot);
+						if (stack.isEmpty())
+							continue;
+						ItemStack remainder = behaviour.handleInsertion(stack, facing, false);
+						if (remainder.equals(stack, false))
+							continue;
+						inventory.setStackInSlot(slot, remainder);
+						changed = true;
+					}
+					if (changed) {
+						markDirty();
+						sendData();
+					}
+					return;
+				}
+			}
+
+			//Eject Items
+			for (int slot = 0; slot < inventory.getSlots(); slot++) {
+				ItemStack stack = inventory.getStackInSlot(slot);
+				if (stack.isEmpty())
+					continue;
+				ItemEntity entityIn = new ItemEntity(world, outPos.x, outPos.y, outPos.z, stack);
+				entityIn.setMotion(outSpeed);
+				entityIn.getPersistentData()
+						.put("BypassCrushingWheel", NBTUtil.writeBlockPos(pos));
+				world.addEntity(entityIn);
+			}
+			inventory.clear();
+			world.notifyBlockUpdate(pos, getBlockState(), getBlockState(), 2 | 16);
+
 
 			return;
 		}
 
 		if (!processingEntity.isAlive() || !processingEntity.getBoundingBox()
-			.intersects(new AxisAlignedBB(pos).grow(.5f))) {
+				.intersects(new AxisAlignedBB(pos).grow(.5f))) {
 			clear();
 			return;
 		}
 
 		double xMotion = ((pos.getX() + .5f) - processingEntity.getX()) / 2f;
+		double yMotion = ((pos.getY() + .5f) - processingEntity.getY()) / 2f;
 		double zMotion = ((pos.getZ() + .5f) - processingEntity.getZ()) / 2f;
 		if (processingEntity.isSneaking())
 			xMotion = zMotion = 0;
-
-		processingEntity.setMotion(new Vec3d(xMotion, Math.max(-speed / 4f, -.5f), zMotion));
+		double movement = Math.max(-speed / 4f, -.5f) * -offset;
+		processingEntity.setMotion(new Vec3d(facing.getAxis() == Axis.X ? movement : xMotion
+				, facing.getAxis() == Axis.Y ? movement : 0f							//Do not move entities upwards or downwards for horizontal crushers,
+				, facing.getAxis() == Axis.Z ? movement : zMotion));					//Or they'll only get their feet crushed.
 
 		if (world.isRemote)
 			return;
 
 		if (!(processingEntity instanceof ItemEntity)) {
 			processingEntity.attackEntityFrom(CrushingWheelTileEntity.damageSource,
-				AllConfigs.SERVER.kinetics.crushingDamage.get());
+					AllConfigs.SERVER.kinetics.crushingDamage.get());
 			if (!processingEntity.isAlive()) {
-				processingEntity.setPosition(outPos.x, outPos.y - .75f, outPos.z);
+				processingEntity.setPosition(outPos.x + (facing.getAxis() == Axis.X ? .75f * offset : 0f)	//This is supposed to move the mobs to the output location
+						, outPos.y + (facing.getAxis() == Axis.Y ? .75f * offset : 0f)						//So the item drops end up on the other end
+						, outPos.z + (facing.getAxis() == Axis.Z ? .75f * offset : 0f));						//This, however, does not currently work consistently for non-downwards crushers.
 			}
 			return;
 		}
 
 		ItemEntity itemEntity = (ItemEntity) processingEntity;
 		itemEntity.setPickupDelay(20);
-		if (processingEntity.getY() < pos.getY() + .25f) {
-			inventory.clear();
-			inventory.setStackInSlot(0, itemEntity.getItem()
-				.copy());
-			itemInserted(inventory.getStackInSlot(0));
-			itemEntity.remove();
-			world.notifyBlockUpdate(pos, getBlockState(), getBlockState(), 2 | 16);
+		if (facing.getAxis() == Axis.Y) {
+			if (processingEntity.getY()  * -offset < (centerPos.y - .25f) * -offset) {
+				intakeItem(itemEntity);
+			}
+		} else if (facing.getAxis() == Axis.Z) {
+			if (processingEntity.getZ()  * -offset < (centerPos.z - .25f) * -offset) {
+				intakeItem(itemEntity);
+			}
+		} else {
+			if (processingEntity.getX()  * -offset < (centerPos.x - .25f) * -offset) {
+				intakeItem(itemEntity);
+			}
 		}
+	}
 
+	private void intakeItem(ItemEntity itemEntity) {
+		inventory.clear();
+		inventory.setStackInSlot(0, itemEntity.getItem()
+				.copy());
+		itemInserted(inventory.getStackInSlot(0));
+		itemEntity.remove();
+		world.notifyBlockUpdate(pos, getBlockState(), getBlockState(), 2 | 16);
 	}
 
 	protected void spawnParticles(ItemStack stack) {
@@ -169,14 +243,14 @@ public class CrushingWheelControllerTileEntity extends SmartTileEntity {
 		IParticleData particleData = null;
 		if (stack.getItem() instanceof BlockItem)
 			particleData = new BlockParticleData(ParticleTypes.BLOCK, ((BlockItem) stack.getItem()).getBlock()
-				.getDefaultState());
+					.getDefaultState());
 		else
 			particleData = new ItemParticleData(ParticleTypes.ITEM, stack);
 
 		Random r = world.rand;
 		for (int i = 0; i < 4; i++)
 			world.addParticle(particleData, pos.getX() + r.nextFloat(), pos.getY() + r.nextFloat(),
-				pos.getZ() + r.nextFloat(), 0, 0, 0);
+					pos.getZ() + r.nextFloat(), 0, 0, 0);
 	}
 
 	private void applyRecipe() {
@@ -185,11 +259,11 @@ public class CrushingWheelControllerTileEntity extends SmartTileEntity {
 		List<ItemStack> list = new ArrayList<>();
 		if (recipe.isPresent()) {
 			int rolls = inventory.getStackInSlot(0)
-				.getCount();
+					.getCount();
 			inventory.clear();
 			for (int roll = 0; roll < rolls; roll++) {
 				List<ItemStack> rolledResults = recipe.get()
-					.rollResults();
+						.rollResults();
 				for (int i = 0; i < rolledResults.size(); i++) {
 					ItemStack stack = rolledResults.get(i);
 					ItemHelper.addToList(stack, list);
@@ -238,7 +312,7 @@ public class CrushingWheelControllerTileEntity extends SmartTileEntity {
 	private void itemInserted(ItemStack stack) {
 		Optional<ProcessingRecipe<RecipeWrapper>> recipe = findRecipe();
 		inventory.remainingTime = recipe.isPresent() ? recipe.get()
-			.getProcessingDuration() : 100;
+				.getProcessingDuration() : 100;
 		inventory.appliedRecipe = false;
 	}
 
