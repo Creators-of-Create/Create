02da3c4c8852777a9329813456bf89844ec5ad4d
s0127943 <bv939@web.de>
2021-02-02 23:21:26 +0100

    Implemented fix by @Snownee for the 1.16 version.

--- src/main/java/com/simibubi/create/content/logistics/block/chute/ChuteTileEntity.java
+++ src/main/java/com/simibubi/create/content/logistics/block/chute/ChuteTileEntity.java
@@ -226,6 +226,8 @@ public class ChuteTileEntity extends SmartTileEntity implements IHaveGoggleInfor
 		AxisAlignedBB searchArea =
 			new AxisAlignedBB(center.add(0, -bottomPullDistance - 0.5, 0), center.add(0, -0.5, 0)).grow(.45f);
 		for (ItemEntity itemEntity : world.getEntitiesWithinAABB(ItemEntity.class, searchArea)) {
+			if (!itemEntity.isAlive())
+				continue;
 			setItem(itemEntity.getItem()
 				.copy(),
 				(float) (itemEntity.getBoundingBox()
