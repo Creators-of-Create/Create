389b2e0e90a26d75e4367f381d89d66156a5b9b2
simibubi <31564874+simibubi@users.noreply.github.com>
2021-03-30 16:57:21 +0200

    Ponder fix-ups
    - Input window elements no longer use integer coordinates
    - Comfy reading is now a config option and persists across ponder UIs
    - Disabled editor mode

--- src/main/java/com/simibubi/create/foundation/config/CClient.java
+++ src/main/java/com/simibubi/create/foundation/config/CClient.java
@@ -3,7 +3,7 @@ package com.simibubi.create.foundation.config;
 public class CClient extends ConfigBase {
 
 	public ConfigGroup client = group(0, "client",
-			"Client-only settings - If you're looking for general settings, look inside your worlds serverconfig folder!");
+		"Client-only settings - If you're looking for general settings, look inside your worlds serverconfig folder!");
 	public ConfigBool tooltips = b(true, "enableTooltips", "Show item descriptions on Shift and controls on Ctrl.");
 	public ConfigBool enableOverstressedTooltip =
 		b(true, "enableOverstressedTooltip", "Display a tooltip when looking at overstressed components.");
@@ -12,13 +12,18 @@ public class CClient extends ConfigBase {
 	public ConfigFloat fanParticleDensity = f(.5f, 0, 1, "fanParticleDensity");
 	public ConfigBool rainbowDebug =
 		b(true, "enableRainbowDebug", "Show colourful debug information while the F3-Menu is open.");
-
 	public ConfigBool experimentalRendering =
 		b(true, "experimentalRendering", "Use modern OpenGL features to drastically increase performance.");
+	public ConfigInt overlayOffsetX = i(20, Integer.MIN_VALUE, Integer.MAX_VALUE, "overlayOffsetX",
+		"Offset the overlay from goggle- and hover- information by this many pixels on the X axis; Use /create overlay");
+	public ConfigInt overlayOffsetY = i(0, Integer.MIN_VALUE, Integer.MAX_VALUE, "overlayOffsetY",
+		"Offset the overlay from goggle- and hover- information by this many pixels on the Y axis; Use /create overlay");
+	public ConfigBool smoothPlacementIndicator = b(false, "smoothPlacementIndicator",
+		"Use an alternative indicator when showing where the assisted placement ends up relative to your crosshair");
 
-	public ConfigInt overlayOffsetX = i(20, Integer.MIN_VALUE, Integer.MAX_VALUE, "overlayOffsetX", "Offset the overlay from goggle- and hover- information by this many pixels on the X axis; Use /create overlay");
-	public ConfigInt overlayOffsetY = i(0, Integer.MIN_VALUE, Integer.MAX_VALUE, "overlayOffsetY", "Offset the overlay from goggle- and hover- information by this many pixels on the Y axis; Use /create overlay");
-	public ConfigBool smoothPlacementIndicator = b(false, "smoothPlacementIndicator", "Use an alternative indicator when showing where the assisted placement ends up relative to your crosshair");
+	public ConfigGroup ponder = group(1, "ponder", "Ponder settings");
+	public ConfigBool comfyReading =
+		b(false, "comfyReading", "Slow down a ponder scene whenever there is text on screen.");
 
 	@Override
 	public String getName() {
