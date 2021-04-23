d2dc576644ece27419f69756bbb92009c56d30b1
grimmauld <soeren@benjos.de>
2021-04-23 09:36:59 +0200

    Forgot some files

--- src/main/java/com/simibubi/create/content/optics/ILightHandler.java
+++ src/main/java/com/simibubi/create/content/optics/ILightHandler.java
@@ -14,7 +14,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.DyeColor;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
