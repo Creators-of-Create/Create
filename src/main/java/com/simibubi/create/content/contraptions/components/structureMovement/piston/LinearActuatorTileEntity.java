e80eeef6ea97c0a1b285e10e3d87195ce0be1b16
reidbhuntley <reidbhuntley@gmail.com>
2021-05-31 15:29:01 -0400

    Improve stability of sequenced gearshift (esp. for rotations)

--- src/main/java/com/simibubi/create/content/contraptions/components/structureMovement/piston/LinearActuatorTileEntity.java
+++ src/main/java/com/simibubi/create/content/contraptions/components/structureMovement/piston/LinearActuatorTileEntity.java
@@ -270,7 +270,7 @@ public abstract class LinearActuatorTileEntity extends KineticTileEntity
 	}
 
 	public float getMovementSpeed() {
-		float movementSpeed = MathHelper.clamp(getSpeed() / 512f, -.49f, .49f) + clientOffsetDiff / 2f;
+		float movementSpeed = MathHelper.clamp(convertToLinear(getSpeed()), -.49f, .49f) + clientOffsetDiff / 2f;
 		if (world.isRemote)
 			movementSpeed *= ServerSpeedProvider.get();
 		return movementSpeed;
