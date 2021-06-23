d189efd987ac3f0945dd476b8492d97134d23d07
JozsefA <jozsefaug@gmail.com>
2021-06-23 00:16:24 -0700

    Whoops missed some classes

---
+++ src/main/java/com/simibubi/create/foundation/data/CreateEntityBuilder.java
@@ -0,0 +1,57 @@
+package com.simibubi.create.foundation.data;
+
+import com.jozufozu.flywheel.backend.instancing.InstancedRenderRegistry;
+import com.jozufozu.flywheel.backend.instancing.entity.IEntityInstanceFactory;
+import com.jozufozu.flywheel.backend.instancing.tile.ITileInstanceFactory;
+import com.tterrag.registrate.AbstractRegistrate;
+import com.tterrag.registrate.builders.BuilderCallback;
+import com.tterrag.registrate.builders.EntityBuilder;
+
+import com.tterrag.registrate.util.OneTimeEventReceiver;
+import com.tterrag.registrate.util.nullness.NonNullSupplier;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class CreateEntityBuilder<T extends Entity, P> extends EntityBuilder<T, P> {
+
+	@Nullable
+	private NonNullSupplier<IEntityInstanceFactory<? super T>> instanceFactory;
+
+	public static <T extends Entity, P> EntityBuilder<T, P> create(AbstractRegistrate<?> owner, P parent, String name, BuilderCallback callback, EntityType.IFactory<T> factory, EntityClassification classification) {
+		return (new CreateEntityBuilder<>(owner, parent, name, callback, factory, classification)).defaultLang();
+	}
+
+	public CreateEntityBuilder(AbstractRegistrate<?> owner, P parent, String name, BuilderCallback callback, EntityType.IFactory<T> factory, EntityClassification classification) {
+		super(owner, parent, name, callback, factory, classification);
+	}
+
+	public CreateEntityBuilder<T, P> instance(NonNullSupplier<IEntityInstanceFactory<? super T>> instanceFactory) {
+		if (this.instanceFactory == null) {
+			DistExecutor.runWhenOn(Dist.CLIENT, () -> this::registerInstance);
+		}
+
+		this.instanceFactory = instanceFactory;
+
+		return this;
+	}
+
+	protected void registerInstance() {
+		OneTimeEventReceiver.addModListener(FMLClientSetupEvent.class, $ -> {
+			NonNullSupplier<IEntityInstanceFactory<? super T>> instanceFactory = this.instanceFactory;
+			if (instanceFactory != null) {
+				InstancedRenderRegistry.getInstance().register(getEntry(), instanceFactory.get());
+			}
+
+		});
+	}
+
+}
