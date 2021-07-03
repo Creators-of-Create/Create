8a598a699b2820268f770c59ac5787a1e174cec6
simibubi <31564874+simibubi@users.noreply.github.com>
2021-07-03 15:37:07 +0200

    Ender Chutes are real
    - Fixed chutes directly between two inventories transferring items instantly
    - Fixed jittering visuals of the clientside chute item near containers
    - Rail blocks can now be picked up with a wrench

--- src/main/java/com/simibubi/create/content/logistics/block/chute/ChuteTileEntity.java
+++ src/main/java/com/simibubi/create/content/logistics/block/chute/ChuteTileEntity.java
@@ -159,18 +159,16 @@ public class ChuteTileEntity extends SmartTileEntity implements IHaveGoggleInfor
 					nextOffset = .5f;
 				else if (nextOffset < 0) {
 					handleDownwardOutput(world.isRemote && !isVirtual());
-					return;
+					nextOffset = itemPosition.value;
 				}
 			}
-		}
-
-		if (itemMotion > 0) {
+		} else if (itemMotion > 0) {
 			if (nextOffset > .5f) {
 				if (!handleUpwardOutput(true))
 					nextOffset = .5f;
 				else if (nextOffset > 1) {
 					handleUpwardOutput(world.isRemote && !isVirtual());
-					return;
+					nextOffset = itemPosition.value;
 				}
 			}
 		}
@@ -317,7 +315,7 @@ public class ChuteTileEntity extends SmartTileEntity implements IHaveGoggleInfor
 		float zOff = Create.RANDOM.nextFloat() * .5f + .25f;
 		Vector3d v = origin.add(xOff, verticalStart, zOff);
 		Vector3d d = origin.add(xOff, verticalEnd, zOff)
-				.subtract(v);
+			.subtract(v);
 		if (Create.RANDOM.nextFloat() < 2 * motion)
 			world.addOptionalParticle(airParticleData, v.x, v.y, v.z, d.x, d.y, d.z);
 	}
@@ -325,25 +323,26 @@ public class ChuteTileEntity extends SmartTileEntity implements IHaveGoggleInfor
 	private void handleInputFromAbove() {
 		if (!capAbove.isPresent())
 			capAbove = grabCapability(Direction.UP);
-		handleInput(capAbove.orElse(null));
+		handleInput(capAbove.orElse(null), 1);
 	}
 
 	private void handleInputFromBelow() {
 		if (!capBelow.isPresent())
 			capBelow = grabCapability(Direction.DOWN);
-		handleInput(capBelow.orElse(null));
+		handleInput(capBelow.orElse(null), 0);
 	}
 
-	private void handleInput(IItemHandler inv) {
+	private void handleInput(IItemHandler inv, float startLocation) {
 		if (inv == null)
 			return;
 		Predicate<ItemStack> canAccept = this::canAcceptItem;
 		int count = getExtractionAmount();
 		ExtractionCountMode mode = getExtractionMode();
 		if (mode == ExtractionCountMode.UPTO || !ItemHelper.extract(inv, canAccept, mode, count, true)
-			.isEmpty())
-			item = ItemHelper.extract(inv, canAccept, mode, count, false);
-
+			.isEmpty()) {
+			ItemStack extracted = ItemHelper.extract(inv, canAccept, mode, count, false);
+			setItem(extracted, startLocation);
+		}
 	}
 
 	private boolean handleDownwardOutput(boolean simulate) {
@@ -356,6 +355,8 @@ public class ChuteTileEntity extends SmartTileEntity implements IHaveGoggleInfor
 		if (!capBelow.isPresent())
 			capBelow = grabCapability(Direction.DOWN);
 		if (capBelow.isPresent()) {
+			if (world.isRemote && !isVirtual())
+				return false;
 			ItemStack remainder = ItemHandlerHelper.insertItemStacked(capBelow.orElse(null), item, simulate);
 			ItemStack held = getItem();
 			if (!simulate)
@@ -408,6 +409,8 @@ public class ChuteTileEntity extends SmartTileEntity implements IHaveGoggleInfor
 			if (!capAbove.isPresent())
 				capAbove = grabCapability(Direction.UP);
 			if (capAbove.isPresent()) {
+				if (world.isRemote && !isVirtual() && !ChuteBlock.isChute(stateAbove))
+					return false;
 				int countBefore = item.getCount();
 				ItemStack remainder = ItemHandlerHelper.insertItemStacked(capAbove.orElse(null), item, simulate);
 				if (!simulate)
