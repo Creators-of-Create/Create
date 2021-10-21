855fc52d6224f75c045085585f4e9aa025e6d38a
simibubi <31564874+simibubi@users.noreply.github.com>
2021-10-21 19:23:49 +0200

    Update FurnaceEngineModifiers.java
    - Use registry delegates
    - Reduce boilerplate

--- src/main/java/com/simibubi/create/content/contraptions/components/flywheel/engine/FurnaceEngineModifiers.java
+++ src/main/java/com/simibubi/create/content/contraptions/components/flywheel/engine/FurnaceEngineModifiers.java
@@ -6,34 +6,27 @@ import java.util.Map;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraftforge.registries.IRegistryDelegate;
 
 public class FurnaceEngineModifiers {
-	
+
 	public final static FurnaceEngineModifiers INSTANCE = new FurnaceEngineModifiers();
-	
-	protected Map<Block, Float> blockModifiers;
-	
-	public FurnaceEngineModifiers(Map<Block, Float> blockModifiers) {
-		this.blockModifiers = blockModifiers;
-	}
-	
-	public FurnaceEngineModifiers() {
-		this(new HashMap<>());
-	}
-	
-	public void set(Block block, float modifier) {
+
+	protected Map<IRegistryDelegate<Block>, Float> blockModifiers = new HashMap<>();
+
+	public void register(IRegistryDelegate<Block> block, float modifier) {
 		this.blockModifiers.put(block, modifier);
 	}
-	
-	public float getModifier(BlockState state, float def) {
-		return blockModifiers.getOrDefault(state.getBlock(), def);
+
+	public float getModifierOrDefault(BlockState state, float defaultValue) {
+		return blockModifiers.getOrDefault(state.getBlock().delegate, defaultValue);
 	}
-	
+
 	public float getModifier(BlockState state) {
-		return getModifier(state, 1f);
+		return getModifierOrDefault(state, 1f);
 	}
-	
+
 	public static void register() {
-		INSTANCE.set(Blocks.BLAST_FURNACE, 2f);
+		INSTANCE.register(Blocks.BLAST_FURNACE.delegate, 2f);
 	}
 }
