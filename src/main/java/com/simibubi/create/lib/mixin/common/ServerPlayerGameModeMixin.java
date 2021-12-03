22f330f8776c29d36c4ca7e6173dca2b68d7daaf
AlphaMode <AlphaModeYT@gmail.com>
2021-12-02 19:46:25 -0600

    Fix block placement

--- src/main/java/com/simibubi/create/lib/mixin/common/ServerPlayerGameModeMixin.java
+++ src/main/java/com/simibubi/create/lib/mixin/common/ServerPlayerGameModeMixin.java
@@ -2,20 +2,27 @@ package com.simibubi.create.lib.mixin.common;
 
 import com.simibubi.create.lib.item.BlockUseBypassingItem;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerPlayerGameMode;
 
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
 import net.minecraft.world.level.block.state.BlockState;
 
 import net.minecraft.world.phys.BlockHitResult;
 
+import com.simibubi.create.lib.item.UseFirstBehaviorItem;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerGameMode.class)
 public class ServerPlayerGameModeMixin {
@@ -26,5 +33,14 @@ public class ServerPlayerGameModeMixin {
 			if (bypassing.shouldBypass(instance, blockHitResult.getBlockPos(), level, player, interactionHand)) return InteractionResult.PASS;
 		}
 		return instance.use(level, player, interactionHand, blockHitResult);
+	}
+
+	@Inject(method = "useItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z", ordinal = 0, shift = At.Shift.BEFORE), cancellable = true)
+	public void create$onItemFirstUse(ServerPlayer serverPlayer, Level level, ItemStack itemStack, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+		if (itemStack.getItem() instanceof UseFirstBehaviorItem first) {
+			UseOnContext useoncontext = new UseOnContext(serverPlayer, interactionHand, blockHitResult);
+			InteractionResult result = first.onItemUseFirst(itemStack, useoncontext);
+			if (result != InteractionResult.PASS) cir.setReturnValue(result);
+		}
 	}
 }
