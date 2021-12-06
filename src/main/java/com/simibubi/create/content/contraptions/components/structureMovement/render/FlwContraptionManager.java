dadf710ee6312790eed0d53e84b25f7dab4b61fc
smellypengu <adrian.rasmussen05@gmail.com>
2021-12-06 21:49:41 +0100

    fix flywheel rendering yes

--- src/main/java/com/simibubi/create/content/contraptions/components/structureMovement/render/FlwContraptionManager.java
+++ src/main/java/com/simibubi/create/content/contraptions/components/structureMovement/render/FlwContraptionManager.java
@@ -7,6 +7,7 @@ import com.jozufozu.flywheel.backend.Backend;
 import com.jozufozu.flywheel.backend.gl.GlTextureUnit;
 import com.jozufozu.flywheel.backend.state.RenderLayer;
 import com.jozufozu.flywheel.event.RenderLayerEvent;
+import com.jozufozu.flywheel.util.TextureBinder;
 import com.simibubi.create.content.contraptions.components.structureMovement.Contraption;
 import com.simibubi.create.foundation.render.AllProgramSpecs;
 import com.simibubi.create.foundation.render.CreateContexts;
@@ -40,7 +41,7 @@ public class FlwContraptionManager extends ContraptionRenderManager<RenderedCont
 
 		layer.setupRenderState();
 
-//		TextureBinder.bindActiveTextures();
+		TextureBinder.bindActiveTextures();
 
 		ContraptionProgram structureShader = CreateContexts.STRUCTURE.getProgram(AllProgramSpecs.STRUCTURE);
 
