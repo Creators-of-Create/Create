5ae4e497878bc14bd40884f355aabd1c3336276a
Jozufozu <jozsefaug@gmail.com>
2021-12-06 23:36:15 -0800

    Get started on a batching engine
     - An Engine is a MaterialManager and a RenderDispatcher
     - Refactor InstanceManager's ctor to use the MaterialManager interface instead of the concrete type
     - MaterialManagerImpl -> InstancingEngine
     - Add skeleton for BatchingEngine
     - Hack in InstanceWorld to switch between the 2
     - Rename/move existing MaterialManager impl to new package

--- src/main/java/com/simibubi/create/content/contraptions/base/flwdata/RotatingType.java
+++ src/main/java/com/simibubi/create/content/contraptions/base/flwdata/RotatingType.java
@@ -2,11 +2,14 @@ package com.simibubi.create.content.contraptions.base.flwdata;
 
 import com.jozufozu.flywheel.backend.gl.attrib.VertexFormat;
 import com.jozufozu.flywheel.backend.gl.buffer.VecBuffer;
+import com.jozufozu.flywheel.backend.struct.Batched;
+import com.jozufozu.flywheel.backend.struct.BatchingTransformer;
 import com.jozufozu.flywheel.backend.struct.StructWriter;
 import com.jozufozu.flywheel.backend.struct.Writeable;
+import com.jozufozu.flywheel.core.model.IModel;
 import com.simibubi.create.foundation.render.AllInstanceFormats;
 
-public class RotatingType implements Writeable<RotatingData> {
+public class RotatingType implements Writeable<RotatingData>, Batched<RotatingData> {
 	@Override
 	public RotatingData create() {
 		return new RotatingData();
@@ -20,5 +23,10 @@ public class RotatingType implements Writeable<RotatingData> {
 	@Override
 	public StructWriter<RotatingData> getWriter(VecBuffer backing) {
 		return new UnsafeRotatingWriter(backing, this);
+	}
+
+	@Override
+	public BatchingTransformer<RotatingData> getTransformer(IModel model) {
+		return null;
 	}
 }
--- src/main/java/com/simibubi/create/content/contraptions/components/structureMovement/render/ContraptionGroup.java
+++ src/main/java/com/simibubi/create/content/contraptions/components/structureMovement/render/ContraptionGroup.java
@@ -1,13 +1,13 @@
 package com.simibubi.create.content.contraptions.components.structureMovement.render;
 
-import com.jozufozu.flywheel.backend.material.MaterialGroupImpl;
-import com.jozufozu.flywheel.backend.material.MaterialManagerImpl;
+import com.jozufozu.flywheel.backend.material.instancing.InstancedMaterialGroup;
+import com.jozufozu.flywheel.backend.material.instancing.InstancingEngine;
 
-public class ContraptionGroup<P extends ContraptionProgram> extends MaterialGroupImpl<P> {
+public class ContraptionGroup<P extends ContraptionProgram> extends InstancedMaterialGroup<P> {
 
 	private final RenderedContraption contraption;
 
-	public ContraptionGroup(RenderedContraption contraption, MaterialManagerImpl<P> owner) {
+	public ContraptionGroup(RenderedContraption contraption, InstancingEngine<P> owner) {
 		super(owner);
 
 		this.contraption = contraption;
@@ -18,7 +18,7 @@ public class ContraptionGroup<P extends ContraptionProgram> extends MaterialGrou
 		contraption.setup(program);
 	}
 
-	public static <P extends ContraptionProgram> MaterialManagerImpl.GroupFactory<P> forContraption(RenderedContraption c) {
+	public static <P extends ContraptionProgram> InstancingEngine.GroupFactory<P> forContraption(RenderedContraption c) {
 		return (materialManager) -> new ContraptionGroup<>(c, materialManager);
 	}
 }
