257767cbec29f5d7a7a8708add0385e925177dc0
simibubi <31564874+simibubi@users.noreply.github.com>
2021-12-13 08:20:16 +0100

    Version up!

--- src/main/java/com/simibubi/create/Create.java
+++ src/main/java/com/simibubi/create/Create.java
@@ -58,7 +58,7 @@ public class Create {
 
 	public static final String ID = "create";
 	public static final String NAME = "Create";
-	public static final String VERSION = "0.4-unstable";
+	public static final String VERSION = "0.4a";
 
 	public static final Logger LOGGER = LogManager.getLogger();
 
