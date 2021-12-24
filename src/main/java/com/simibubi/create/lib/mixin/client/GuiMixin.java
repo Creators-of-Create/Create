dc2f2fb82bfc1d6800a717c1d389ee9992afa53c
Platymemo <edwardhazelton+github@gmail.com>
2021-12-23 17:24:33 -0900

    Fixed goggles messing up the crosshair texture

--- src/main/java/com/simibubi/create/lib/mixin/client/GuiMixin.java
+++ src/main/java/com/simibubi/create/lib/mixin/client/GuiMixin.java
@@ -1,5 +1,7 @@
 package com.simibubi.create.lib.mixin.client;
 
+import static net.minecraft.client.gui.GuiComponent.GUI_ICONS_LOCATION;
+
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -8,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.lib.event.OverlayRenderCallback;
 
@@ -15,6 +18,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GameRenderer;
 
 @Environment(EnvType.CLIENT)
 @Mixin(Gui.class)
@@ -42,5 +46,9 @@ public abstract class GuiMixin {
 			method = "renderCrosshair")
 	private void create$renderCrosshair(PoseStack matrixStack, CallbackInfo ci) {
 		OverlayRenderCallback.EVENT.invoker().onOverlayRender(matrixStack, create$partialTicks, minecraft.getWindow(), OverlayRenderCallback.Types.CROSSHAIRS);
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+		RenderSystem.setShader(GameRenderer::getPositionTexShader);
+		RenderSystem.setShaderTexture(0, GUI_ICONS_LOCATION);
+		RenderSystem.enableBlend();
 	}
 }
