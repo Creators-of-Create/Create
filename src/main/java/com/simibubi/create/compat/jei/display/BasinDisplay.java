f17cc59a1a79761df595c23c2cf79514b5c0835e
Aeiou <3160746+AeiouEnigma@users.noreply.github.com>
2021-12-25 17:26:23 -0500

    BasinDisplay but it's a skeleton we probably don't actually need

--- src/main/java/com/simibubi/create/compat/jei/display/BasinDisplay.java
+++ src/main/java/com/simibubi/create/compat/jei/display/BasinDisplay.java
@@ -1,29 +1,10 @@
-//package com.simibubi.create.compat.jei.display;
-//
-//import com.simibubi.create.content.contraptions.processing.BasinRecipe;
-//
-//import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-//import me.shedaniel.rei.api.common.entry.EntryIngredient;
-//
-//import java.util.List;
-//
-//public class BasinDisplay extends AbstractCreateDisplay<BasinRecipe> {
-//	public BasinDisplay(BasinRecipe recipe) {
-//		super(recipe);
-//	}
-//
-//	@Override
-//	public List<EntryIngredient> getInputEntries() {
-//		return getRecipe().getIngredients();
-//	}
-//
-//	@Override
-//	public List<EntryIngredient> getOutputEntries() {
-//		return null;
-//	}
-//
-//	@Override
-//	public CategoryIdentifier<?> getCategoryIdentifier() {
-//		return null;
-//	}
-//}
+package com.simibubi.create.compat.jei.display;
+
+import com.simibubi.create.content.contraptions.processing.BasinRecipe;
+
+// Is this class even necessary? Couldn't we just have MixingDisplay and PackingDisplay classes instead?
+public abstract class BasinDisplay extends AbstractCreateDisplay<BasinRecipe> {
+	public BasinDisplay(BasinRecipe recipe) {
+		super(recipe);
+	}
+}
