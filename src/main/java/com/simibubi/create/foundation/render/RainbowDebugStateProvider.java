74a147db302f822496b4b2471d78c7695524256d
Jozufozu <jozsefaug@gmail.com>
2022-01-12 00:20:45 -0800

    Remove "states" field from program jsons

--- src/main/java/com/simibubi/create/foundation/render/RainbowDebugStateProvider.java
+++ src/main/java/com/simibubi/create/foundation/render/RainbowDebugStateProvider.java
@@ -1,27 +1,31 @@
 package com.simibubi.create.foundation.render;
 
+import javax.annotation.Nonnull;
+
+import com.jozufozu.flywheel.core.compile.ShaderConstants;
 import com.jozufozu.flywheel.core.shader.GameStateProvider;
 import com.simibubi.create.Create;
 import com.simibubi.create.content.contraptions.KineticDebugger;
 
 import net.minecraft.resources.ResourceLocation;
 
-public class RainbowDebugStateProvider implements GameStateProvider {
-
-	public static final RainbowDebugStateProvider INSTANCE = new RainbowDebugStateProvider();
+public enum RainbowDebugStateProvider implements GameStateProvider {
+	INSTANCE;
 	public static final ResourceLocation NAME = Create.asResource("rainbow_debug");
-
-	protected RainbowDebugStateProvider() {
-
-	}
 
 	@Override
 	public boolean isTrue() {
 		return KineticDebugger.isActive();
 	}
 
+	@Nonnull
 	@Override
 	public ResourceLocation getID() {
 		return NAME;
+	}
+
+	@Override
+	public void alterConstants(@Nonnull ShaderConstants constants) {
+		constants.define("DEBUG_RAINBOW");
 	}
 }
