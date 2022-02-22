6788389cb221eb9777e121a5e387575b72d6919e
TropheusJ <jverrellijr@icloud.com>
2022-02-21 20:11:24 -0500

    probably fix issues with entity packets

--- src/main/java/com/simibubi/create/lib/mixin/client/ClientPacketListenerMixin.java
+++ src/main/java/com/simibubi/create/lib/mixin/client/ClientPacketListenerMixin.java
@@ -1,10 +1,19 @@
 package com.simibubi.create.lib.mixin.client;
 
+import com.simibubi.create.lib.entity.ExtraSpawnDataEntity;
+import com.simibubi.create.lib.util.EntityHelper;
+
+import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
+
+import net.minecraft.world.entity.Entity;
+
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.simibubi.create.lib.block.CustomDataPacketHandlingBlockEntity;
@@ -16,12 +25,28 @@ import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
 @Environment(EnvType.CLIENT)
 @Mixin(ClientPacketListener.class)
 public abstract class ClientPacketListenerMixin {
 	@Shadow
 	@Final
 	private Connection connection;
+
+	@ModifyArgs(
+			method = "handleAddEntity",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/multiplayer/ClientLevel;putNonPlayerEntity(ILnet/minecraft/world/entity/Entity;)V"
+			)
+	)
+	private void create$handleAddEntity(Args args) {
+		Entity entity = args.get(1);
+		if (entity instanceof ExtraSpawnDataEntity) {
+			EntityHelper.entityLoad(entity);
+		}
+	}
 
 	@Inject(method = "method_38542", at = @At("HEAD"), cancellable = true)
 	public void create$handleCustomBlockEntity(ClientboundBlockEntityDataPacket packet, BlockEntity blockEntity, CallbackInfo ci) {
--- src/main/java/com/simibubi/create/lib/util/EntityHelper.java
+++ src/main/java/com/simibubi/create/lib/util/EntityHelper.java
@@ -19,12 +19,18 @@ import net.minecraft.network.protocol.Packet;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public final class EntityHelper {
 	public static final String EXTRA_DATA_KEY = "create_ExtraEntityData";
 	public static final ResourceLocation EXTRA_DATA_PACKET = Create.asResource("extra_data");
+	private static final Map<Integer, FriendlyByteBuf> QUEUED_DATA = new HashMap<>(); // can't guarantee that extra data is received after main packet
 
+	/**
+	 * Extra data sent from server to client.
+	 */
 	public static void handleDataSend(Entity entity, Consumer<Packet<?>> callback) {
 		if (entity instanceof ExtraSpawnDataEntity extra) {
 			FriendlyByteBuf data = PacketByteBufs.create();
@@ -37,22 +43,45 @@ public final class EntityHelper {
 		}
 	}
 
+	/**
+	 * Extra data received on client.
+	 * If the entity exists, the main spawn packet was received first and data
+	 * should be loaded immediately.
+	 * If it does not exist yet, that means the extra data was received first.
+	 * It will be stored in a map until the main spawn packet is received.
+	 */
 	@Environment(EnvType.CLIENT)
 	public static void handleDataReceive(Minecraft client, ClientPacketListener handler, FriendlyByteBuf buf, PacketSender responseSender) {
 		int id = buf.readVarInt();
 		FriendlyByteBuf copy = PacketByteBufs.copy(buf); // need to copy so the data survives client.execute()
 		client.execute(() -> {
 			Entity entity = handler.getLevel().getEntity(id);
-			if (entity instanceof ExtraSpawnDataEntity extra) {
-				extra.readSpawnData(copy);
+			if (entity == null) {
+				QUEUED_DATA.put(id, copy); // entity does not exist yet - save for later
+			} else if (entity instanceof ExtraSpawnDataEntity extra) {
+				extra.readSpawnData(copy); // entity exists - load now
+				copy.release();
 			} else {
-				if (entity == null) {
-					Create.LOGGER.error("Tried to send extra data to entity [{}], which doesn't exist!", id);
-				} else {
-					Create.LOGGER.error("Tried to send extra data for an entity that doesn't implement ExtraSpawnDataEntity");
-				}
+				Create.LOGGER.error("Tried to receive extra data for an entity that doesn't implement ExtraSpawnDataEntity");
 			}
 		});
+	}
+
+	/**
+	 * Invoked when main entity packet is received.
+	 * data stored by handleDataReceive will be loaded here if present.
+	 */
+	public static void entityLoad(Entity entity) {
+		if (entity instanceof ExtraSpawnDataEntity extra) {
+			int id = entity.getId();
+			FriendlyByteBuf data = QUEUED_DATA.remove(id);
+			if (data != null) {
+				extra.readSpawnData(data);
+				data.release();
+			}
+		} else {
+			Create.LOGGER.error("Tried to receive extra data for an entity that doesn't implement ExtraSpawnDataEntity");
+		}
 	}
 
 	@Environment(EnvType.CLIENT)
