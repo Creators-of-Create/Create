5fe27cf1abb323148192084748e30afe3079d5fb
PepperCode1 <44146161+PepperCode1@users.noreply.github.com>
2022-02-23 22:21:39 -0800

    Fix #142
    - The chunk update tag was being read as a server-side update instead of
    a client-side update meaning that an update for the Flywheel instance
    was not enqueued.

--- src/main/java/com/simibubi/create/lib/mixin/common/LevelChunkMixin.java
+++ src/main/java/com/simibubi/create/lib/mixin/common/LevelChunkMixin.java
@@ -1,23 +1,28 @@
 package com.simibubi.create.lib.mixin.common;
 
-import com.simibubi.create.lib.extensions.ChunkUnloadListeningBlockEntity;
-
-import net.minecraft.core.Registry;
-import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.LevelHeightAccessor;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.chunk.ChunkAccess;
-import net.minecraft.world.level.chunk.LevelChunk;
-
-import net.minecraft.world.level.chunk.LevelChunkSection;
-import net.minecraft.world.level.chunk.UpgradeData;
-import net.minecraft.world.level.levelgen.blending.BlendingData;
-
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import com.simibubi.create.lib.block.CustomUpdateTagHandlingBlockEntity;
+import com.simibubi.create.lib.extensions.ChunkUnloadListeningBlockEntity;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.LevelHeightAccessor;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.chunk.LevelChunkSection;
+import net.minecraft.world.level.chunk.UpgradeData;
+import net.minecraft.world.level.levelgen.blending.BlendingData;
 
 @Mixin(LevelChunk.class)
 public abstract class LevelChunkMixin extends ChunkAccess {
@@ -32,5 +37,13 @@ public abstract class LevelChunkMixin extends ChunkAccess {
 				listener.onChunkUnloaded();
 			}
 		});
+	}
+
+	@Inject(method = "method_31716(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntityType;Lnet/minecraft/nbt/CompoundTag;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/BlockEntity;load(Lnet/minecraft/nbt/CompoundTag;)V"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+	private void create$handleBlockEntityUpdateTag(BlockPos pos, BlockEntityType<?> type, CompoundTag tag, CallbackInfo ci, BlockEntity blockEntity) {
+		if (blockEntity instanceof CustomUpdateTagHandlingBlockEntity handler) {
+			handler.handleUpdateTag(tag);
+			ci.cancel();
+		}
 	}
 }
