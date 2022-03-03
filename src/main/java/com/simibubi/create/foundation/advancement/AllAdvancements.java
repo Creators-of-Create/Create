d0d6d360d20a383316df43d9779c5bf08fe0f92f
lcy0x1 <arthurlcy0x1@gmail.com>
2022-03-03 13:45:42 -0800

    reduce white space diff

--- src/main/java/com/simibubi/create/foundation/advancement/AllAdvancements.java
+++ src/main/java/com/simibubi/create/foundation/advancement/AllAdvancements.java
@@ -366,6 +366,7 @@ public class AllAdvancements implements DataProvider {
 		Advancement deployer =
 			kinecticAdvancement("deployer", AllBlocks.DEPLOYER.get(), TaskType.MILESTONE).parent(brass_casing)
 				.save(t, id + ":deployer");
+
 		Advancement clockwork_component =
 			itemAdvancement("precision_mechanism", AllItems.PRECISION_MECHANISM, TaskType.NORMAL).parent(deployer)
 				.save(t, id + ":precision_mechanism");
@@ -373,6 +374,7 @@ public class AllAdvancements implements DataProvider {
 		Advancement clockwork_component_eob = deadEnd().parent(clockwork_component)
 			.addCriterion("0", itemGathered(AllItems.PRECISION_MECHANISM.get()))
 			.save(t, id + ":clockwork_component_eob");
+
 		Advancement extendo_grip =
 			advancement("extendo_grip", AllItems.EXTENDO_GRIP.get(), TaskType.NORMAL).parent(clockwork_component)
 				.addCriterion("0", AllTriggers.EXTENDO.instance())
