d8ecb44e9d67678d6f384bb36164b9d3dbb8a375
TropheusJ <jverrellijr@icloud.com>
2022-03-08 17:33:42 -0500

    fix ghosts

--- src/main/java/com/simibubi/create/foundation/utility/ghost/GhostBlockRenderer.java
+++ src/main/java/com/simibubi/create/foundation/utility/ghost/GhostBlockRenderer.java
@@ -1,6 +1,5 @@
 package com.simibubi.create.foundation.utility.ghost;
 
-import java.util.List;
 import java.util.Random;
 
 import com.jozufozu.flywheel.core.virtual.VirtualEmptyBlockGetter;
@@ -9,9 +8,6 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.simibubi.create.foundation.render.SuperRenderTypeBuffer;
 import com.simibubi.create.foundation.utility.placement.PlacementHelpers;
-
-import io.github.fabricators_of_create.porting_lib.model.EmptyModelData;
-import io.github.fabricators_of_create.porting_lib.model.IModelData;
 import io.github.fabricators_of_create.porting_lib.render.FixedLightBakedModel;
 import io.github.fabricators_of_create.porting_lib.render.TranslucentBakedModel;
 
@@ -20,15 +16,9 @@ import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
-import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.util.Mth;
-import net.minecraft.world.level.block.state.BlockState;
-
-import javax.annotation.Nullable;
 
 public abstract class GhostBlockRenderer {
 
@@ -63,11 +53,9 @@ public abstract class GhostBlockRenderer {
 			BlockPos pos = params.pos;
 			ms.translate(pos.getX(), pos.getY(), pos.getZ());
 
-//			dispatcher.getModelRenderer()
-//				.renderModel(ms.last(), vb, params.state, model, 1f, 1f, 1f, LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY);
 			model = DefaultLayerFilteringBakedModel.wrap(model);
 			dispatcher.getModelRenderer()
-				.tesselateBlock(VirtualEmptyBlockGetter.FULL_BRIGHT, model, params.state, pos, ms, vb, false, new Random(), 42L, OverlayTexture.NO_OVERLAY);
+					.tesselateBlock(VirtualEmptyBlockGetter.FULL_BRIGHT, model, params.state, pos, ms, vb, false, new Random(), 42L, OverlayTexture.NO_OVERLAY);
 
 			ms.popPose();
 		}
@@ -95,58 +83,13 @@ public abstract class GhostBlockRenderer {
 			ms.scale(.85f, .85f, .85f);
 			ms.translate(-.5, -.5, -.5);
 
-			float alpha = params.alphaSupplier.get() * .75f * PlacementHelpers.getCurrentAlpha();
-			renderModel(ms.last(), vb, params.state, model, 1f, 1f, 1f, alpha,
-					LevelRenderer.getLightColor(mc.level, pos), OverlayTexture.NO_OVERLAY,
-					EmptyModelData.INSTANCE);
+			model = DefaultLayerFilteringBakedModel.wrap(model);
+			model = FixedLightBakedModel.wrap(model, LevelRenderer.getLightColor(mc.level, pos));
+			model = TranslucentBakedModel.wrap(model, () -> params.alphaSupplier.get() * .75f * PlacementHelpers.getCurrentAlpha());
+			dispatcher.getModelRenderer()
+					.tesselateBlock(VirtualEmptyBlockGetter.INSTANCE, model, params.state, pos, ms, vb, false, new Random(), 42L, OverlayTexture.NO_OVERLAY);
 
 			ms.popPose();
-		}
-
-		// ModelBlockRenderer
-		public void renderModel(PoseStack.Pose pose, VertexConsumer consumer,
-								@Nullable BlockState state, BakedModel model, float red, float green, float blue,
-								float alpha, int packedLight, int packedOverlay, IModelData modelData) {
-			Random random = new Random();
-
-			for (Direction direction : Direction.values()) {
-				random.setSeed(42L);
-				renderQuadList(pose, consumer, red, green, blue, alpha,
-					model.getQuads(state, direction, random), packedLight, packedOverlay);
-			}
-
-			random.setSeed(42L);
-			renderQuadList(pose, consumer, red, green, blue, alpha,
-				model.getQuads(state, null, random), packedLight, packedOverlay);
-		}
-
-		// ModelBlockRenderer
-		private static void renderQuadList(PoseStack.Pose pose, VertexConsumer consumer,
-			float red, float green, float blue, float alpha, List<BakedQuad> quads,
-			int packedLight, int packedOverlay) {
-			for (BakedQuad quad : quads) {
-				float f;
-				float f1;
-				float f2;
-				if (quad.isTinted()) {
-					f = Mth.clamp(red, 0.0F, 1.0F);
-					f1 = Mth.clamp(green, 0.0F, 1.0F);
-					f2 = Mth.clamp(blue, 0.0F, 1.0F);
-				} else {
-					f = 1.0F;
-					f1 = 1.0F;
-					f2 = 1.0F;
-				}
-
-				consumer.putBulkData(
-						pose, quad,
-						new float[] { f, f1, f2, alpha },
-						1f, 1f, 1f,
-						new int[] { packedLight, packedLight, packedLight, packedLight },
-						packedOverlay, false
-				);
-			}
-
 		}
 
 	}
