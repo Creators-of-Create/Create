dbc8d7586602ed0e6bb5597b47bb3541c2a20f92
TropheusJ <jverrellijr@icloud.com>
2022-04-09 15:45:33 -0400

    fix #241

--- src/main/java/com/simibubi/create/content/contraptions/processing/BasinInventory.java
+++ src/main/java/com/simibubi/create/content/contraptions/processing/BasinInventory.java
@@ -1,30 +1,34 @@
 package com.simibubi.create.content.contraptions.processing;
 
 import com.simibubi.create.foundation.item.SmartInventory;
-import io.github.fabricators_of_create.porting_lib.transfer.item.ItemHandlerHelper;
 
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext;
-import net.minecraft.world.item.ItemStack;
 
 public class BasinInventory extends SmartInventory {
 
 	private BasinTileEntity te;
+	private boolean output;
 
-	public BasinInventory(int slots, BasinTileEntity te) {
+	public BasinInventory(int slots, BasinTileEntity te, boolean output) {
 		super(slots, te, 16, true);
 		this.te = te;
+		this.output = output;
 	}
 
 	@Override
 	public long insert(ItemVariant resource, long maxAmount, TransactionContext transaction) {
 		// Only insert if no other slot already has a stack of this item
-		try (Transaction t = transaction.openNested()) {
-			long extracted = extract(resource, 1, t);
-			t.abort();
-			if (extracted != 0)
-				return 0;
+		// fabric: on forge this excludes the slot being inserted to. We don't deal with slots,
+		// so we allow any insertion into the output inv for hopefully identical behavior.
+		if (!output) {
+			try (Transaction t = transaction.openNested()) {
+				long extracted = extract(resource, 1, t);
+				t.abort();
+				if (extracted != 0)
+					return 0;
+			}
 		}
 		return super.insert(resource, maxAmount, transaction);
 	}
--- src/main/java/com/simibubi/create/content/contraptions/processing/BasinTileEntity.java
+++ src/main/java/com/simibubi/create/content/contraptions/processing/BasinTileEntity.java
@@ -121,9 +121,9 @@ public class BasinTileEntity extends SmartTileEntity implements IHaveGoggleInfor
 
 	public BasinTileEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
 		super(type, pos, state);
-		inputInventory = new BasinInventory(9, this);
+		inputInventory = new BasinInventory(9, this, false);
 		inputInventory.whenContentsChanged(() -> contentsChanged = true);
-		outputInventory = new BasinInventory(9, this).forbidInsertion()
+		outputInventory = new BasinInventory(9, this, true).forbidInsertion()
 			.withMaxStackSize(64);
 		areFluidsMoving = false;
 		itemCapability = new CombinedStorage<>(List.of(inputInventory, outputInventory));
