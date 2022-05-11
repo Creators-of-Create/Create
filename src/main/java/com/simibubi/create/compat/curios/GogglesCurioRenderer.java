2ed5ee9f3b274f036f5080358cd302d26cfef0e1
patryk3211 <patrykmierzy@gmail.com>
2022-05-11 14:42:43 +0200

    Goggles are on your eyes if you wear something on your head.

--- src/main/java/com/simibubi/create/compat/curios/GogglesCurioRenderer.java
+++ src/main/java/com/simibubi/create/compat/curios/GogglesCurioRenderer.java
@@ -18,6 +18,7 @@ import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
@@ -52,6 +53,11 @@ public class GogglesCurioRenderer implements ICurioRenderer {
 		matrixStack.translate(0, -0.25, 0);
 		matrixStack.mulPose(Vector3f.ZP.rotationDegrees(180.0f));
 		matrixStack.scale(0.625f, 0.625f, 0.625f);
+
+		if(!slotContext.entity().getItemBySlot(EquipmentSlot.HEAD).isEmpty()) {
+			matrixStack.mulPose(Vector3f.ZP.rotationDegrees(180.0f));
+			matrixStack.translate(0, -0.25, 0);
+		}
 
 		// Render
 		Minecraft.getInstance().getItemRenderer().renderStatic(stack, ItemTransforms.TransformType.HEAD, light, OverlayTexture.NO_OVERLAY, matrixStack, renderTypeBuffer, 0);
