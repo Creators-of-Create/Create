5af222a99b84c1e66075cfa94983bc383a2c9fcb
patryk3211 <patrykmierzy@gmail.com>
2022-05-11 14:23:55 +0200

    Goggles render correctly when worn as a Curio

---
+++ src/main/java/com/simibubi/create/compat/curios/CuriosRenderers.java
@@ -0,0 +1,21 @@
+package com.simibubi.create.compat.curios;
+
+import com.simibubi.create.AllItems;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
+
+@OnlyIn(Dist.CLIENT)
+public class CuriosRenderers {
+	public static void register() {
+		CuriosRendererRegistry.register(AllItems.GOGGLES.get(), () -> new GogglesCurioRenderer(Minecraft.getInstance().getEntityModels().bakeLayer(GogglesCurioRenderer.LAYER)));
+	}
+
+	public static void onLayerRegister(final EntityRenderersEvent.RegisterLayerDefinitions event) {
+		event.registerLayerDefinition(GogglesCurioRenderer.LAYER, () -> LayerDefinition.create(GogglesCurioRenderer.mesh(), 1, 1));
+	}
+}
