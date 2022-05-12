27b7f9c7bdd39066f34606b55ad0a7aa3fb61b16
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2022-05-12 03:33:58 +0200

    Sticky mess!
    - added `/create glue fill|clear <cuboid> [axes]` command to fill/clear cuboid with glue
    - added `/create glue floodfill <source> [limit] [mask]` command to propagate glue to neighbors
    - added GlueHelper as an API
    
    todo: better floodfill masking, cuboid fill flag to glue exposed faces

--- src/main/java/com/simibubi/create/foundation/command/GlueCommand.java
+++ src/main/java/com/simibubi/create/foundation/command/GlueCommand.java
@@ -1,32 +1,73 @@
 package com.simibubi.create.foundation.command;
 
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.ArgumentBuilder;
-import com.simibubi.create.content.contraptions.components.structureMovement.glue.SuperGlueEntity;
-
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.simibubi.create.foundation.utility.GlueHelper;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.Commands;
-import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.level.block.state.pattern.BlockInWorld;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.EnumSet;
+import java.util.function.Predicate;
+
+import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
+import static net.minecraft.commands.Commands.argument;
+import static net.minecraft.commands.Commands.literal;
+import static net.minecraft.commands.arguments.blocks.BlockPredicateArgument.blockPredicate;
+import static net.minecraft.commands.arguments.blocks.BlockPredicateArgument.getBlockPredicate;
+import static net.minecraft.commands.arguments.coordinates.BlockPosArgument.blockPos;
+import static net.minecraft.commands.arguments.coordinates.BlockPosArgument.getLoadedBlockPos;
+import static net.minecraft.commands.arguments.coordinates.SwizzleArgument.getSwizzle;
+import static net.minecraft.commands.arguments.coordinates.SwizzleArgument.swizzle;
 
 public class GlueCommand {
-	public static ArgumentBuilder<CommandSourceStack, ?> register() {
-		return Commands.literal("glue")
-				.requires(cs -> cs.hasPermission(2))
-				.then(Commands.argument("pos", BlockPosArgument.blockPos())
-						//.then(Commands.argument("direction", EnumArgument.enumArgument(Direction.class))
-								.executes(ctx -> {
-									BlockPos pos = BlockPosArgument.getLoadedBlockPos(ctx, "pos");
 
-									ServerLevel world = ctx.getSource().getLevel();
-									SuperGlueEntity entity = new SuperGlueEntity(world, pos, Direction.UP);
+    public static final int GLUE_LIMIT = 32768;
 
-									entity.playPlaceSound();
-									world.addFreshEntity(entity);
+    public static ArgumentBuilder<CommandSourceStack, ?> register() {
+        return literal("glue")
+            .requires(cs -> cs.hasPermission(2))
+            .then(literal("fill")
+                .then(argument("pos1", blockPos()).then(argument("pos2", blockPos())
+                    .executes(ctx -> fillCommand(ctx, EnumSet.allOf(Direction.Axis.class)))
+                    .then(argument("axes", swizzle())//enumArgument(Direction.Axis.class))
+                        .executes(ctx -> fillCommand(ctx, getSwizzle(ctx, "axes")))))))
+            .then(literal("clear")
+                .then(argument("pos1", blockPos()).then(argument("pos2", blockPos())
+                    .executes(ctx -> clearCommand(ctx, EnumSet.allOf(Direction.Axis.class)))
+                    .then(argument("axes", swizzle())
+                        .executes(ctx -> clearCommand(ctx, getSwizzle(ctx, "axes")))))))
+            .then(literal("floodfill")
+                .then(argument("source", blockPos())
+                    .executes(ctx -> floodfillCommand(ctx, GLUE_LIMIT, null))
+                    .then(argument("limit", integer(1, GLUE_LIMIT))
+                        .executes(ctx -> floodfillCommand(ctx, GLUE_LIMIT, null))
+                        .executes(ctx -> floodfillCommand(ctx, ctx.getArgument("limit", Integer.class), null))
+                        .then(argument("mask", blockPredicate())
+                            .executes(ctx -> floodfillCommand(ctx, ctx.getArgument("limit", Integer.class), getBlockPredicate(ctx, "mask")))))))
+            ;
+    }
 
-									return 1;
-								}));
+    private static int fillCommand(CommandContext<CommandSourceStack> ctx, @NotNull EnumSet<Direction.Axis> axes) throws CommandSyntaxException {
+        int cleared = GlueHelper.fill(ctx.getSource().getLevel(), getLoadedBlockPos(ctx, "pos1"), getLoadedBlockPos(ctx, "pos2"), axes);
+        ctx.getSource().sendSuccess(new TextComponent("Successfully applied " + cleared + " patches of glue."), true);
+        return Command.SINGLE_SUCCESS;
+    }
 
-	}
+    private static int clearCommand(CommandContext<CommandSourceStack> ctx, @NotNull EnumSet<Direction.Axis> axes) throws CommandSyntaxException {
+        int cleared = GlueHelper.clear(ctx.getSource().getLevel(), getLoadedBlockPos(ctx, "pos1"), getLoadedBlockPos(ctx, "pos2"), axes);
+        ctx.getSource().sendSuccess(new TextComponent("Successfully cleared " + cleared + " patches of glue."), true);
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int floodfillCommand(CommandContext<CommandSourceStack> ctx, int limit, @Nullable Predicate<BlockInWorld> mask) throws CommandSyntaxException {
+        int propagated = GlueHelper.floodfill(ctx.getSource().getLevel(), getLoadedBlockPos(ctx, "source"), limit, mask);
+        ctx.getSource().sendSuccess(new TextComponent("Successfully propagated " + propagated + "/" + limit + " patches of glue."), true);
+        return Command.SINGLE_SUCCESS;
+    }
 }
