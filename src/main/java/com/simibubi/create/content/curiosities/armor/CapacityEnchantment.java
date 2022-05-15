74a4b16b59435247475f5b201db11bdb0e29f17f
TropheusJ <jverrellijr@icloud.com>
2022-05-14 23:04:45 -0400

    fix #288

--- src/main/java/com/simibubi/create/content/curiosities/armor/CapacityEnchantment.java
+++ src/main/java/com/simibubi/create/content/curiosities/armor/CapacityEnchantment.java
@@ -1,17 +1,15 @@
 package com.simibubi.create.content.curiosities.armor;
 
-import com.simibubi.create.AllItems;
-import io.github.fabricators_of_create.porting_lib.util.EnchantmentUtil;
-
+import io.github.fabricators_of_create.porting_lib.enchant.CustomEnchantingTableBehaviorEnchantment;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
-public class CapacityEnchantment extends Enchantment {
+public class CapacityEnchantment extends Enchantment implements CustomEnchantingTableBehaviorEnchantment {
 
 	public CapacityEnchantment(Rarity p_i46731_1_, EnchantmentCategory p_i46731_2_, EquipmentSlot[] p_i46731_3_) {
 		super(p_i46731_1_, p_i46731_2_, p_i46731_3_);
-		EnchantmentUtil.addCompat(AllItems.COPPER_BACKTANK.get(), () -> this);
 	}
 
 	@Override
@@ -19,10 +17,15 @@ public class CapacityEnchantment extends Enchantment {
 		return 3;
 	}
 
-//	@Override
-//	public boolean canApplyAtEnchantingTable(ItemStack stack) {
-//		return stack.getItem() instanceof ICapacityEnchantable;
-//	}
+	@Override
+	public boolean canApplyAtEnchantingTable(ItemStack stack) {
+		return stack.getItem() instanceof ICapacityEnchantable;
+	}
+
+	@Override
+	public boolean canEnchant(ItemStack stack) {
+		return canApplyAtEnchantingTable(stack);
+	}
 
 	public interface ICapacityEnchantable {
 	}
--- src/main/java/com/simibubi/create/content/curiosities/weapons/PotatoRecoveryEnchantment.java
+++ src/main/java/com/simibubi/create/content/curiosities/weapons/PotatoRecoveryEnchantment.java
@@ -1,17 +1,15 @@
 package com.simibubi.create.content.curiosities.weapons;
 
-import com.simibubi.create.AllItems;
-import io.github.fabricators_of_create.porting_lib.util.EnchantmentUtil;
-
+import io.github.fabricators_of_create.porting_lib.enchant.CustomEnchantingTableBehaviorEnchantment;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
-public class PotatoRecoveryEnchantment extends Enchantment {
+public class PotatoRecoveryEnchantment extends Enchantment implements CustomEnchantingTableBehaviorEnchantment {
 
 	public PotatoRecoveryEnchantment(Rarity p_i46731_1_, EnchantmentCategory p_i46731_2_, EquipmentSlot[] p_i46731_3_) {
 		super(p_i46731_1_, p_i46731_2_, p_i46731_3_);
-		EnchantmentUtil.addCompat(AllItems.POTATO_CANNON.get(), () -> this);
 	}
 
 	@Override
@@ -19,9 +17,13 @@ public class PotatoRecoveryEnchantment extends Enchantment {
 		return 3;
 	}
 
-//	@Override
-//	public boolean canApplyAtEnchantingTable(ItemStack stack) {
-//		return stack.getItem() instanceof PotatoCannonItem;
-//	}
+	@Override
+	public boolean canApplyAtEnchantingTable(ItemStack stack) {
+		return stack.getItem() instanceof PotatoCannonItem;
+	}
 
+	@Override
+	public boolean canEnchant(ItemStack stack) {
+		return canApplyAtEnchantingTable(stack);
+	}
 }
