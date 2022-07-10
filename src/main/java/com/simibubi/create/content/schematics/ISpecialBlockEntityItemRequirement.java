a6b35f2a8d96fc130b2d54a35f426323b4f70e26
simibubi <31564874+simibubi@users.noreply.github.com>
2022-07-10 18:39:37 +0200

    Schematic item requirement fix
    - ISpecialXItemRequirement no longer provides a default implementation

--- src/main/java/com/simibubi/create/content/schematics/ISpecialBlockEntityItemRequirement.java
+++ src/main/java/com/simibubi/create/content/schematics/ISpecialBlockEntityItemRequirement.java
@@ -4,8 +4,6 @@ import net.minecraft.world.level.block.state.BlockState;
 
 public interface ISpecialBlockEntityItemRequirement {
 
-	default ItemRequirement getRequiredItems(BlockState state) {
-		return ItemRequirement.INVALID;
-	}
+	public ItemRequirement getRequiredItems(BlockState state);
 
 }
