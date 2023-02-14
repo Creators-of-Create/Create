454a0f05b7ca24cfe2eb6afb1e4dde6a4c8923e8
Nova_starter <cyvack@outlook.com>
2023-02-13 18:44:41 -0800

    Add Javadoc to IEncasable and IEncased interfaces

--- src/main/java/com/simibubi/create/content/contraptions/relays/elementary/IEncasable.java
+++ src/main/java/com/simibubi/create/content/contraptions/relays/elementary/IEncasable.java
@@ -14,10 +14,20 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
+/**
+ * Implement this interface to indicate that a block should be encasable
+ */
 public interface IEncasable {
 
+	/**
+	 * Handles how encasement should be done if tryEncase is successful
+	 */
 	void handleEncasing(BlockState state, Level level, BlockPos pos, Block encasedBlock);
 
+	/**
+	 * Implement this method into your overridden use method.
+	 * @return If the Interaction result was a success, or pass
+	 */
 	default InteractionResult tryEncase(BlockState state, Level level, BlockPos pos,ItemStack heldItem) {
 		ArrayList<Block> encasedBlocks = Encasable.encasableBlocks.get(state.getBlock());
 		for (Block block : encasedBlocks) {
--- src/main/java/com/simibubi/create/content/contraptions/relays/elementary/IEncased.java
+++ src/main/java/com/simibubi/create/content/contraptions/relays/elementary/IEncased.java
@@ -2,6 +2,9 @@ package com.simibubi.create.content.contraptions.relays.elementary;
 
 import com.simibubi.create.content.contraptions.base.CasingBlock;
 
+/**
+ * Implement this interface to indicate that this block is an EncasedBlock
+ */
 public interface IEncased {
 	CasingBlock getCasing();
 	void setCasing(CasingBlock casing);
