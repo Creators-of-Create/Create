9355f84b7a3a61ed2755e23a2179f45395b921e1
XanderWander <xander.dekeijzer@student.hu.nl>
2023-02-26 00:17:10 +0100

    Changed max track placement length from 128 to 256.

--- src/main/java/com/simibubi/create/foundation/config/CTrains.java
+++ src/main/java/com/simibubi/create/foundation/config/CTrains.java
@@ -3,21 +3,21 @@ package com.simibubi.create.foundation.config;
 public class CTrains extends ConfigBase {
 
 	public final ConfigBool trainsCauseDamage = b(true, "trainsCauseDamage", Comments.trainsCauseDamage);
-	public final ConfigInt maxTrackPlacementLength = i(32, 16, 128, "maxTrackPlacementLength", Comments.maxTrackPlacementLength);
+	public final ConfigInt maxTrackPlacementLength = i(32, 16, 256, "maxTrackPlacementLength", Comments.maxTrackPlacementLength);
 	public final ConfigInt maxAssemblyLength = i(128, 5, "maxAssemblyLength", Comments.maxAssemblyLength);
 	public final ConfigInt maxBogeyCount = i(20, 1, "maxBogeyCount", Comments.maxBogeyCount);
 	public final ConfigFloat manualTrainSpeedModifier = f(.75f, 0, "manualTrainSpeedModifier", Comments.manualTrainSpeedModifier);
-	
+
 	public final ConfigGroup trainStats = group(1, "trainStats", "Standard Trains");
 	public final ConfigFloat trainTopSpeed = f(28, 0, "trainTopSpeed", Comments.mps, Comments.trainTopSpeed);
 	public final ConfigFloat trainTurningTopSpeed = f(14, 0, "trainTurningTopSpeed", Comments.mps, Comments.trainTurningTopSpeed);
 	public final ConfigFloat trainAcceleration = f(3, 0, "trainAcceleration", Comments.acc, Comments.trainAcceleration);
-	
+
 	public final ConfigGroup poweredTrainStats = group(1, "poweredTrainStats", "Powered Trains");
 	public final ConfigFloat poweredTrainTopSpeed = f(40, 0, "poweredTrainTopSpeed", Comments.mps, Comments.poweredTrainTopSpeed);
 	public final ConfigFloat poweredTrainTurningTopSpeed = f(20, 0, "poweredTrainTurningTopSpeed", Comments.mps, Comments.poweredTrainTurningTopSpeed);
 	public final ConfigFloat poweredTrainAcceleration = f(3, 0, "poweredTrainAcceleration", Comments.acc, Comments.poweredTrainAcceleration);
-	
+
 
 	@Override
 	public String getName() {
