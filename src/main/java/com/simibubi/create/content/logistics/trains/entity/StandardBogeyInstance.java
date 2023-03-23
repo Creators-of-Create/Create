469d9d592b9aa925b26b1206a4ad03d73d422a92
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-03-23 17:42:28 +0000

    Added Standard Bogey Instance (Might be redundant)

---
+++ src/main/java/com/simibubi/create/content/logistics/trains/entity/StandardBogeyInstance.java
@@ -0,0 +1,20 @@
+package com.simibubi.create.content.logistics.trains.entity;
+
+import com.jozufozu.flywheel.api.MaterialManager;
+import com.simibubi.create.content.logistics.trains.BogeyRenderer;
+import com.simibubi.create.content.logistics.trains.StandardBogeyRenderer;
+
+public class StandardBogeyInstance extends BogeyInstance {
+	public StandardBogeyInstance(CarriageBogey bogey, BogeyRenderer.BogeySize bogeySize, MaterialManager materialManager) {
+		super(bogey, new StandardBogeyRenderer(), bogeySize, materialManager);
+	}
+
+	public static StandardBogeyInstance drive(CarriageBogey bogey, MaterialManager materialManager) {
+		return new StandardBogeyInstance(bogey, BogeyRenderer.BogeySize.LARGE, materialManager);
+	}
+
+
+	public static StandardBogeyInstance frame(CarriageBogey bogey, MaterialManager materialManager) {
+		return new StandardBogeyInstance(bogey, BogeyRenderer.BogeySize.SMALL, materialManager);
+	}
+}
