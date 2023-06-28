cdab3b5f663440dfbb85423dc23aaccf16f6c3a8
attackeight <attackeight@gmail.com>
2023-06-28 11:21:32 -0400

    Add IC2 Metals compat

--- src/main/java/com/simibubi/create/foundation/data/recipe/CompatMetals.java
+++ src/main/java/com/simibubi/create/foundation/data/recipe/CompatMetals.java
@@ -1,5 +1,6 @@
 package com.simibubi.create.foundation.data.recipe;
 
+import static com.simibubi.create.foundation.data.recipe.Mods.IC2;
 import static com.simibubi.create.foundation.data.recipe.Mods.IE;
 import static com.simibubi.create.foundation.data.recipe.Mods.MEK;
 import static com.simibubi.create.foundation.data.recipe.Mods.TH;
@@ -7,15 +8,15 @@ import static com.simibubi.create.foundation.data.recipe.Mods.TH;
 import com.simibubi.create.foundation.utility.Lang;
 
 public enum CompatMetals {
-	ALUMINUM(IE),
+	ALUMINUM(IE, IC2),
 	LEAD(MEK, TH, IE),
 	NICKEL(TH, IE),
 	OSMIUM(MEK),
 	PLATINUM(),
 	QUICKSILVER(),
-	SILVER(TH, IE),
-	TIN(TH, MEK),
-	URANIUM(MEK, IE);
+	SILVER(TH, IE, IC2),
+	TIN(TH, MEK, IC2),
+	URANIUM(MEK, IE, IC2);
 
 	private final Mods[] mods;
 	private final String name;
