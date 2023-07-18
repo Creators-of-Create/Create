57a8c23effbbede2ac71681aee49d3d2bbab32f7
rhysdh540 <rhys_dehaan@ryecountryday.org>
2023-07-18 10:23:24 -0400

    undo changes from other pr
    This reverts commits 7e4a084e and 65c30ccb

--- src/main/java/com/simibubi/create/foundation/config/ConfigBase.java
+++ src/main/java/com/simibubi/create/foundation/config/ConfigBase.java
@@ -22,8 +22,6 @@ public abstract class ConfigBase {
 	protected List<ConfigBase> children;
 
 	public void registerAll(final ForgeConfigSpec.Builder builder) {
-		if(allValues == null) // avoid null pointer exception if config class is empty
-			allValues = new ArrayList<>();
 		for (CValue<?, ?> cValue : allValues)
 			cValue.register(builder);
 	}
