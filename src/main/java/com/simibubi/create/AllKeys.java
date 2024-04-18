971fae9c0edbe12c52d845f5f5eee7f919182a7b
attackeight <attackeight@gmail.com>
2024-04-17 21:22:16 -0400

    Allow Ponder to be re-bound

--- src/main/java/com/simibubi/create/AllKeys.java
+++ src/main/java/com/simibubi/create/AllKeys.java
@@ -9,11 +9,14 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraftforge.client.ClientRegistry;
 
+import org.lwjgl.opengl.GL;
+
 public enum AllKeys {
 
 	TOOL_MENU("toolmenu", GLFW.GLFW_KEY_LEFT_ALT),
 	ACTIVATE_TOOL("", GLFW.GLFW_KEY_LEFT_CONTROL),
 	TOOLBELT("toolbelt", GLFW.GLFW_KEY_LEFT_ALT),
+	PONDER("ponder", GLFW.GLFW_KEY_W)
 
 	;
 
