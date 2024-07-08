4486b16917a9e0af257766544153f8e4c899159f
IThundxr <me@ithundxr.dev>
2024-07-08 18:37:14 -0400

    fix schematic visual not disappearing after item dropped or unfocused in hotbar

--- src/main/java/com/simibubi/create/content/schematics/client/SchematicHandler.java
+++ src/main/java/com/simibubi/create/content/schematics/client/SchematicHandler.java
@@ -161,7 +161,7 @@ public class SchematicHandler implements IGuiOverlay {
 		BlockPos pos;
 
 		pos = BlockPos.ZERO;
-		
+
 		try {
 			schematic.placeInWorld(w, pos, pos, placementSettings, w.getRandom(), Block.UPDATE_CLIENTS);
 		} catch (Exception e) {
@@ -314,8 +314,12 @@ public class SchematicHandler implements IGuiOverlay {
 
 	private boolean itemLost(Player player) {
 		for (int i = 0; i < Inventory.getSelectionSize(); i++) {
+			if (player.getInventory()
+					.getItem(i)
+					.is(activeSchematicItem.getItem()))
+				continue;
 			if (!ItemStack.matches(player.getInventory()
-				.getItem(i), activeSchematicItem))
+					.getItem(i), activeSchematicItem))
 				continue;
 			return false;
 		}
