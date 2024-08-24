08f8d3b0f5d42418f15e78fa238dfe8a80a3046b
IThundxr <me@ithundxr.dev>
2024-08-24 10:46:45 -0400

    extra check to make sure pipes don't place fluids they shouldn't

--- src/main/java/com/simibubi/create/content/fluids/OpenEndedPipe.java
+++ src/main/java/com/simibubi/create/content/fluids/OpenEndedPipe.java
@@ -37,6 +37,7 @@ import net.minecraft.world.level.block.CampfireBlock;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.FlowingFluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.AABB;
@@ -187,6 +188,8 @@ public class OpenEndedPipe extends FlowSource {
 			.isReplaceable())
 			return false;
 		if (fluid.isEmpty())
+			return false;
+		if (!(fluid.getFluid() instanceof FlowingFluid))
 			return false;
 		if (!FluidHelper.hasBlockState(fluid.getFluid()))
 			return true;
