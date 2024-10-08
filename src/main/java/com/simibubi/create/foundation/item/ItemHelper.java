8e50ad4cfd286dcef741da4c47c5b5161972243a
simibubi <31564874+simibubi@users.noreply.github.com>
2024-10-08 16:43:50 +0200

    Stack of Spades
    - Protect item handlers against invalid item stack sizes
    - Fix some held or dropped items not updating count when inserted into item handlers

--- src/main/java/com/simibubi/create/foundation/item/ItemHelper.java
+++ src/main/java/com/simibubi/create/foundation/item/ItemHelper.java
@@ -276,4 +276,16 @@ public class ItemHelper {
 		}
 		return -1;
 	}
+	
+	public static ItemStack limitCountToMaxStackSize(ItemStack stack, boolean simulate) {
+		int count = stack.getCount();
+		int max = stack.getMaxStackSize();
+		if (count <= max)
+			return ItemStack.EMPTY;
+		ItemStack remainder = ItemHandlerHelper.copyStackWithSize(stack, count - max);
+		if (!simulate)
+			stack.setCount(max);
+		return remainder;
+	}
+	
 }
