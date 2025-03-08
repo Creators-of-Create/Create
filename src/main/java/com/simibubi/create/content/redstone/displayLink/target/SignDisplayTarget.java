7a3517faf29351a7b8cb71f1c31b80dcab21c136
dan96kid <dan96kid@gmail.com>
2025-03-08 11:33:09 -0600

    Updated SignDisplayTarget.java again
    - Removed unused import statement.
    - Wrapped all code in the if statements with {}.
    - Removed a seemingly unnecessary `sendBlockUpdated`.

--- src/main/java/com/simibubi/create/content/redstone/displayLink/target/SignDisplayTarget.java
+++ src/main/java/com/simibubi/create/content/redstone/displayLink/target/SignDisplayTarget.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.simibubi.create.api.behaviour.display.DisplayTarget;
 import com.simibubi.create.content.redstone.displayLink.DisplayLinkContext;
 
-import net.createmod.catnip.data.Iterate;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
@@ -16,28 +15,31 @@ public class SignDisplayTarget extends DisplayTarget {
 	@Override
 	public void acceptText(int line, List<MutableComponent> text, DisplayLinkContext context) {
 		BlockEntity be = context.getTargetBlockEntity();
-		if (!(be instanceof SignBlockEntity sign))
+		if (!(be instanceof SignBlockEntity sign)){
 			return;
+		}
 
 		boolean changed = false;
 		SignText signTextFront = ((SignBlockEntity) be).getFrontText();
 		SignText signTextBack = ((SignBlockEntity) be).getBackText();
 		for (int i = 0; i < text.size() && i + line < 4; i++) {
-			if (i == 0)
+			if (i == 0){
 				reserve(i + line, sign, context);
-			if (i > 0 && isReserved(i + line, sign, context))
+			}
+			if (i > 0 && isReserved(i + line, sign, context)){
 				break;
+			}
 			
 			signTextFront = signTextFront.setMessage(i + line, text.get(i));
 			signTextBack = signTextBack.setMessage(i + line, text.get(i));
 			changed = true;
 		}
 
-		if (changed)
+		if (changed){
 			sign.setText(signTextFront, true);
-			context.level().sendBlockUpdated(context.getTargetPos(), sign.getBlockState(), sign.getBlockState(), 2);
 			sign.setText(signTextBack, false);
 			context.level().sendBlockUpdated(context.getTargetPos(), sign.getBlockState(), sign.getBlockState(), 2);
+		}
 	}
 
 	@Override
