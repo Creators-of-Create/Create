d4e7f0582154db4b7cfc927952458c247bb8a2cf
ForgeStove <ForgeStove@outlook.com>
2025-03-09 03:50:29 +0800

    fix server issue

--- src/main/java/com/simibubi/create/content/logistics/packagerLink/LogisticallyLinkedBlockItem.java
+++ src/main/java/com/simibubi/create/content/logistics/packagerLink/LogisticallyLinkedBlockItem.java
@@ -1,12 +1,6 @@
 package com.simibubi.create.content.logistics.packagerLink;
-
 import java.util.List;
 import java.util.UUID;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.phys.HitResult.Type;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -20,16 +14,21 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.ClipContext.Fluid;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult.Type;
 
 public class LogisticallyLinkedBlockItem extends BlockItem {
 
@@ -74,14 +73,13 @@ public class LogisticallyLinkedBlockItem extends BlockItem {
 
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
 		ItemStack itemstack = player.getItemInHand(usedHand);
-		Minecraft mc = Minecraft.getInstance();
+		if (level.isClientSide) return InteractionResultHolder.pass(itemstack);
+		BlockHitResult hitResult = getPlayerPOVHitResult(level, player, Fluid.NONE);
 		if (!itemstack.getComponents().has(DataComponents.BLOCK_ENTITY_DATA))
 			return InteractionResultHolder.fail(itemstack);
-		if (player.isShiftKeyDown() && (mc.hitResult == null || mc.hitResult.getType() == Type.MISS)) {
+		if (player.isShiftKeyDown() && hitResult.getType() == Type.MISS)
 			itemstack.remove(DataComponents.BLOCK_ENTITY_DATA);
-			return InteractionResultHolder.success(itemstack);
-		}
-		return InteractionResultHolder.pass(itemstack);
+		return InteractionResultHolder.success(itemstack);
 	}
 
 	@Override
