bcb28544d7dc8a58c0eb35caf7e234afc572e709
RaymondBlaze <raymond-blaze@outlook.com>
2025-03-25 01:18:42 +0800

    Revert "Skip fluid render for Spout with empty FluidStack"
    This reverts commit c2b2c2d95d72f3a0dda52977e1f7bcd6f9a1b76a.

--- src/main/java/com/simibubi/create/compat/jei/category/animations/AnimatedSpout.java
+++ src/main/java/com/simibubi/create/compat/jei/category/animations/AnimatedSpout.java
@@ -63,11 +63,6 @@ public class AnimatedSpout extends AnimatedKinetics {
 			.scale(scale)
 			.render(graphics);
 
-		if (fluid.isEmpty()) {
-			matrixStack.popPose();
-			return;
-		}
-
 		AnimatedKinetics.DEFAULT_LIGHTING.applyLighting();
 		matrixStack.pushPose();
 		UIRenderHelper.flipForGuiRender(matrixStack);
