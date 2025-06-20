28009840618f0f72f73455bd2dea565856b94a56
ks <2777389616@qq.com>
2025-06-20 20:58:44 +0800

    fixed threshold switch not counting stacks correctly.
    cherry-picked from my previous 1.21.1 branch
    
    https://github.com/Creators-of-Create/Create/issues/7688#issue-2894355570

--- src/main/java/com/simibubi/create/content/redstone/thresholdSwitch/ThresholdSwitchBlockEntity.java
+++ src/main/java/com/simibubi/create/content/redstone/thresholdSwitch/ThresholdSwitchBlockEntity.java
@@ -123,7 +123,6 @@ public class ThresholdSwitchBlockEntity extends SmartBlockEntity {
 	}
 
 	public void updateCurrentLevel() {
-		boolean changed = false;
 		int prevLevel = currentLevel;
 		int prevMaxLevel = currentMaxLevel;
 
@@ -137,66 +136,68 @@ public class ThresholdSwitchBlockEntity extends SmartBlockEntity {
 			currentMinLevel = observable.getMinValue();
 			currentLevel = observable.getCurrentValue();
 			currentMaxLevel = observable.getMaxValue();
-
-		/*} else if (StorageDrawers.isDrawer(targetBlockEntity) && observedInventory.hasInventory()) {
-			currentMinLevel = 0;
-			currentLevel = StorageDrawers.getItemCount(observedInventory.getInventory(), filtering);
-			currentMaxLevel = StorageDrawers.getTotalStorageSpace(observedInventory.getInventory());
-		*/
-
-		} else if (observedInventory.hasInventory() || observedTank.hasInventory()) {
+		} else if (observedInventory.hasInventory()) {
 			currentMinLevel = 0;
 			currentLevel = 0;
 			currentMaxLevel = 0;
 
-			if (observedInventory.hasInventory()) {
+			IItemHandler inv = observedInventory.getInventory();
+			if (invVersionTracker.stillWaiting(inv)) {
+				currentLevel = prevLevel;
+				currentMaxLevel = prevMaxLevel;
+			} else {
+				invVersionTracker.awaitNewVersion(inv);
+				for (int slot = 0; slot < inv.getSlots(); slot++) {
+					ItemStack stackInSlot = inv.getStackInSlot(slot);
 
-				// Item inventory
-				IItemHandler inv = observedInventory.getInventory();
-				if (invVersionTracker.stillWaiting(inv)) {
-					currentLevel = prevLevel;
-					currentMaxLevel = prevMaxLevel;
+					final int slotClone = slot;
+					long space = COMPAT.stream()
+						.filter(compat -> compat.isFromThisMod(targetBlockEntity))
+						.map(compat -> compat.getSpaceInSlot(inv, slotClone))
+						.findFirst()
+						.orElseGet(() -> (long) Math.min(stackInSlot.getMaxStackSize(), inv.getSlotLimit(slotClone)));
 
-				} else {
-					invVersionTracker.awaitNewVersion(inv);
-					for (int slot = 0; slot < inv.getSlots(); slot++) {
-						ItemStack stackInSlot = inv.getStackInSlot(slot);
+					if (space == 0) continue;
 
-						int finalSlot = slot;
-						long space = COMPAT
-							.stream()
-							.filter(compat -> compat.isFromThisMod(targetBlockEntity))
-							.map(compat -> compat.getSpaceInSlot(inv, finalSlot))
-							.findFirst()
-							.orElseGet(() -> (long) Math.min(stackInSlot.getMaxStackSize(), inv.getSlotLimit(finalSlot)));
+					if (inStacks) {
+						// When counting in stacks, each slot has 1 unit of capacity
+						currentMaxLevel += 1;
 
-						int count = stackInSlot.getCount();
-						if (space == 0)
-							continue;
-
+						if (filtering.test(stackInSlot)) {
+							// only count full stacks as 1, partial stacks as 0
+							if (stackInSlot.getCount() == space) {
+								currentLevel += 1;
+							}
+						}
+					} else {
+						// When counting individual items, use actual item capacity
 						currentMaxLevel += space;
-						if (filtering.test(stackInSlot))
-							currentLevel += count;
+
+						// Count items that pass the filter
+						if (filtering.test(stackInSlot)) {
+							currentLevel += stackInSlot.getCount();
+						}
 					}
 				}
 			}
+		} else if (observedTank.hasInventory()) {
+			currentMinLevel = 0;
+			currentLevel = 0;
+			currentMaxLevel = 0;
 
-			if (observedTank.hasInventory()) {
-				// Fluid inventory
-				IFluidHandler tank = observedTank.getInventory();
-				for (int slot = 0; slot < tank.getTanks(); slot++) {
-					FluidStack stackInSlot = tank.getFluidInTank(slot);
-					int space = tank.getTankCapacity(slot);
-					int count = stackInSlot.getAmount();
-					if (space == 0)
-						continue;
+			// Fluid inventory
+			IFluidHandler tank = observedTank.getInventory();
+			for (int slot = 0; slot < tank.getTanks(); slot++) {
+				FluidStack stackInSlot = tank.getFluidInTank(slot);
+				int space = tank.getTankCapacity(slot);
+				int count = stackInSlot.getAmount();
+				if (space == 0) continue;
 
-					currentMaxLevel += space;
-					if (filtering.test(stackInSlot))
-						currentLevel += count;
+				currentMaxLevel += space;
+				if (filtering.test(stackInSlot)) {
+					currentLevel += count;
 				}
 			}
-
 		} else {
 			// No compatible inventories found
 			currentMinLevel = -1;
@@ -213,7 +214,8 @@ public class ThresholdSwitchBlockEntity extends SmartBlockEntity {
 		}
 
 		currentLevel = Mth.clamp(currentLevel, currentMinLevel, currentMaxLevel);
-		changed = currentLevel != prevLevel;
+		boolean levelChanged = currentLevel != prevLevel;
+		boolean anyChanged = levelChanged || currentMaxLevel != prevMaxLevel || currentMinLevel != prevLevel;
 
 		boolean previouslyPowered = redstoneState;
 		if (redstoneState && currentLevel <= offWhenBelow)
@@ -232,10 +234,11 @@ public class ThresholdSwitchBlockEntity extends SmartBlockEntity {
 		if (update)
 			scheduleBlockTick();
 
-		if (changed || update) {
+		if (levelChanged || update)
 			DisplayLinkBlock.notifyGatherers(level, worldPosition);
+
+		if (anyChanged)
 			notifyUpdate();
-		}
 	}
 
 	private boolean isSuitableInventory(BlockEntity be) {
