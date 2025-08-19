4f3638914fc8cb67fef5e5e038f45a835830344b
fabien-gigante <fabien.gigante@free.fr>
2025-08-19 17:17:08 +0200

    Update ValueBoxRenderer.java

--- src/main/java/com/simibubi/create/foundation/blockEntity/behaviour/ValueBoxRenderer.java
+++ src/main/java/com/simibubi/create/foundation/blockEntity/behaviour/ValueBoxRenderer.java
@@ -1,25 +1,19 @@
 package com.simibubi.create.foundation.blockEntity.behaviour;
 
-import org.joml.Matrix3f;
-
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.simibubi.create.content.kinetics.simpleRelays.AbstractSimpleShaftBlock;
-
 import dev.engine_room.flywheel.lib.transform.TransformStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.util.Mth;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.FenceBlock;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class ValueBoxRenderer {
 
