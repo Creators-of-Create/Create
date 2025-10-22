78201e94d766951f0fe79c3d46de9c3e14eb9301
VoidLeech <ian.gerritse@gmail.com>
2025-10-23 01:29:51 +0200

    feat: plough black/whitelist tags instead
    fix: ploughs breaking end portals/gateways

--- src/main/java/com/simibubi/create/content/contraptions/actors/plough/PloughMovementBehaviour.java
+++ src/main/java/com/simibubi/create/content/contraptions/actors/plough/PloughMovementBehaviour.java
@@ -1,5 +1,6 @@
 package com.simibubi.create.content.contraptions.actors.plough;
 
+import com.simibubi.create.AllTags.AllBlockTags;
 import com.simibubi.create.content.contraptions.actors.plough.PloughBlock.PloughFakePlayer;
 import com.simibubi.create.content.contraptions.behaviour.MovementContext;
 import com.simibubi.create.content.kinetics.base.BlockBreakingMovementBehaviour;
@@ -107,13 +108,13 @@ public class PloughMovementBehaviour extends BlockBreakingMovementBehaviour {
 			return false;
 		if (state.getBlock() instanceof BubbleColumnBlock)
 			return false;
-		if (state.getBlock() instanceof NetherPortalBlock)
-			return false;
 		if (state.getBlock() instanceof ITrackBlock)
 			return true;
 		if (state.getBlock() instanceof FakeTrackBlock)
 			return false;
-		if (state.getBlock() instanceof SnowLayerBlock)
+		if (AllBlockTags.PLOUGH_BLACKLIST.matches(state.getBlock()))
+			return false;
+		if (AllBlockTags.PLOUGH_WHITELIST.matches(state.getBlock()))
 			return true;
 		return state.getCollisionShape(world, breakingPos)
 			.isEmpty();
