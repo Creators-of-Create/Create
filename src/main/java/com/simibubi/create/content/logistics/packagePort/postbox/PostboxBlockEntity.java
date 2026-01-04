997feb8ad8ab4c2844e554273c56a0032693f050
techno-sam <77073745+techno-sam@users.noreply.github.com>
2026-01-03 23:37:36 -0800

    Principle of Postbox Preservation
    fixes #8759

--- src/main/java/com/simibubi/create/content/logistics/packagePort/postbox/PostboxBlockEntity.java
+++ src/main/java/com/simibubi/create/content/logistics/packagePort/postbox/PostboxBlockEntity.java
@@ -90,7 +90,12 @@ public class PostboxBlockEntity extends PackagePortBlockEntity {
 
 	@Override
 	protected void onOpenChange(boolean open) {
-		level.setBlockAndUpdate(worldPosition, getBlockState().setValue(PostboxBlock.OPEN, open));
+		// cached getBlockState doesn't update if we're exploded in the meantime, refreshBlockState crashes validation
+		BlockState state = level.getBlockState(worldPosition);
+		if (!(state.getBlock() instanceof PostboxBlock))
+			return;
+
+		level.setBlockAndUpdate(worldPosition, state.setValue(PostboxBlock.OPEN, open));
 		level.playSound(null, worldPosition, open ? SoundEvents.BARREL_OPEN : SoundEvents.BARREL_CLOSE,
 			SoundSource.BLOCKS);
 	}
