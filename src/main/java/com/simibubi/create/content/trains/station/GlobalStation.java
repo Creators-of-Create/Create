4dd0f1613671965bce84e4941b8edc74afbf5ebe
HB-Stratos <56313836+HB-Stratos@users.noreply.github.com>
2026-01-16 12:01:50 +0100

    Fix trains not being detected at station bug: after train pathfinding destination change, stale nearestTrain weakReference was left at station, for which the distanceToDestination value is not valid.

--- src/main/java/com/simibubi/create/content/trains/station/GlobalStation.java
+++ src/main/java/com/simibubi/create/content/trains/station/GlobalStation.java
@@ -121,8 +121,11 @@ public class GlobalStation extends SingleBlockEntityEdgePoint {
 	}
 
 	public void reserveFor(Train train) {
+		if (train == null) return;
+
 		Train nearestTrain = getNearestTrain();
 		if (nearestTrain == null
+			|| nearestTrain.getCurrentStation() != this //distanceToDestination invalid for stale nearestTrain weakReference
 			|| nearestTrain.navigation.distanceToDestination > train.navigation.distanceToDestination)
 			this.nearestTrain = new WeakReference<>(train);
 	}
