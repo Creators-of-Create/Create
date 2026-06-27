54a2b5fdd9f0e904f4711f08dbc37e36d533260b
Groupix05 <90351454+Groupix05@users.noreply.github.com>
2026-06-27 20:09:46 +0200

    Update src/main/java/com/simibubi/create/compat/dynamictrees/DynamicTree.java
    Co-authored-by: Hubble+Koda <hubblethewolverine@gmail.com>

--- src/main/java/com/simibubi/create/compat/dynamictrees/DynamicTree.java
+++ src/main/java/com/simibubi/create/compat/dynamictrees/DynamicTree.java
@@ -17,6 +17,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.LevelEvent;
 
 public class DynamicTree extends AbstractBlockBreakQueue {
 
